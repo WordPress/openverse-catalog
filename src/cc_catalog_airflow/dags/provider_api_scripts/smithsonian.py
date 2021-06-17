14c87fe4059d8f7ebf671a573ab1bddf4e8a003b
Olga Bulat <obulat@gmail.com>
2021-06-17 15:34:12 +0300

    Fix smithsonian logger format
    Signed-off-by: Olga Bulat <obulat@gmail.com>

--- src/cc_catalog_airflow/dags/provider_api_scripts/smithsonian.py
+++ src/cc_catalog_airflow/dags/provider_api_scripts/smithsonian.py
@@ -472,7 +472,7 @@ def _process_image_list(
 
 if __name__ == '__main__':
     logging.basicConfig(
-        format='{asctime} - {name} - {levelname}:  {message}',
+        format='%(asctime)s - %(name)s - %(levelname)s:  %(message)s',
         level=logging.INFO
     )
     main()
