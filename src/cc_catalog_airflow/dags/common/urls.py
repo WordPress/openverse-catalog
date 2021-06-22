83c74cdad00f00a95a67f6ca41510464155b682e
Olga Bulat <obulat@gmail.com>
2021-06-22 08:28:08 +0300

    Use correct comparison
    Co-authored-by: Krystle Salazar <krystle.salazar@ciens.ucv.ve>

--- src/cc_catalog_airflow/dags/common/urls.py
+++ src/cc_catalog_airflow/dags/common/urls.py
@@ -64,7 +64,7 @@ def rewrite_redirected_url(url_string):
         response = requests.get(url_string)
         if response.ok:
             rewritten_url = response.url
-            if rewritten_url != response.url:
+            if rewritten_url != url_string:
                 logger.info(f'{url_string} was rewritten to {rewritten_url}')
         else:
             logger.warning(
