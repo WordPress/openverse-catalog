959dd6e7598dda5dd826c248b199183cea74aee6
Madison Swain-Bowden <bowdenm@spu.edu>
2021-10-13 09:44:28 -0700

    Fix delayed requester test

--- tests/dags/common/test_requester.py
+++ tests/dags/common/test_requester.py
@@ -12,7 +12,7 @@ def test_get_waits_before_getting(monkeypatch):
     def mock_requests_get(url, params, **kwargs):
         return requests.Response()
 
-    monkeypatch.setattr(requester.requests, "get", mock_requests_get)
+    monkeypatch.setattr(requester.requests.Session, "get", mock_requests_get)
     dq = requester.DelayedRequester(delay)
     s = time.time()
     dq.get("https://google.com")
