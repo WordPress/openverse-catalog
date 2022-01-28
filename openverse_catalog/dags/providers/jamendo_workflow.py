1644169df41a1a2925c4fa4f668a90a79d48c2ba
Madison Swain-Bowden <bowdenm@spu.edu>
2022-01-28 13:06:55 -0800

    Add media types to the other audio DAGs

--- openverse_catalog/dags/providers/jamendo_workflow.py
+++ openverse_catalog/dags/providers/jamendo_workflow.py
@@ -18,5 +18,5 @@ globals()[DAG_ID] = create_provider_api_workflow(
     schedule_string="@monthly",
     dated=False,
     dagrun_timeout=DAGRUN_TIMEOUT,
-    media_types="audio",
+    media_types=["audio"],
 )
