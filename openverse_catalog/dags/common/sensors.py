2b6ca8c87a6e311577114354ef6f8cc46d5a7343
Staci Cooper <staci.cooper@automattic.com>
2022-03-03 19:27:08 -0800

    Make the ExternalDAGsSensor deferrable
    We need to run the data refresh DAGs in a pool with only one slot
    in order to make sure no trigger tasks run simultaneously. This
    means that the sensor tasks MUST free up the worker slot while
    waiting, or the entire system will simply deadlock on a DAG that
    is waiting for its siblings to finish.

--- openverse_catalog/dags/common/sensors.py
+++ openverse_catalog/dags/common/sensors.py
@@ -1,17 +1,20 @@
 import os
+from datetime import timedelta
 from typing import Iterable
 
 from airflow.exceptions import AirflowException
 from airflow.models import DagModel, DagRun
 from airflow.sensors.base import BaseSensorOperator
+from airflow.triggers.temporal import TimeDeltaTrigger
 from airflow.utils.session import provide_session
 from airflow.utils.state import State
 from sqlalchemy import func
 
 
-class ExternalDAGsSensor(BaseSensorOperator):
+class ExternalDAGsSensorAsync(BaseSensorOperator):
     """
-    Waits for a list of different DAGs to not be running.
+    Waits for a list of different DAGs to not be running, freeing up
+    a worker slot while it's waiting.
 
     external_dag_ids: A list of dag_ids that you want to wait for
     :param check_existence: Set to `True` to check if the external task exists (when
@@ -43,13 +46,13 @@ class ExternalDAGsSensor(BaseSensorOperator):
         self._has_checked_existence = False
 
     @provide_session
-    def poke(self, context, session=None):
+    def execute(self, context, event=None, session=None):
         self.log.info(
             "Poking for DAGS %s ... ",
             self.external_dag_ids,
         )
 
-        # In poke mode this will check DAG existence only once
+        # Check DAG existence only once, on the first execution.
         if self.check_existence and not self._has_checked_existence:
             self._check_for_existence(session=session)
 
@@ -62,8 +65,16 @@ class ExternalDAGsSensor(BaseSensorOperator):
             .scalar()
         )
 
-        # Success when there are no running DAGs with the provided IDs.
-        return count_running == 0
+        # If there are running DAGs, trigger deferral of the Sensor so that
+        # the worker slot will be freed and the pool does not become
+        # deadlocked waiting on this task.
+        if count_running == 0:
+            self.defer(
+                trigger=TimeDeltaTrigger(
+                    timedelta(minutes=5)
+                ),  # TODO what's a good delta
+                method_name="execute",
+            )
 
     def _check_for_existence(self, session) -> None:
         for dag_id in self.external_dag_ids:
