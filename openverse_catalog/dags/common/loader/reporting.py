4e71d51677071327c778cba40d62210294136d65
Staci Cooper <staci.cooper@automattic.com>
2022-03-21 12:00:05 -0700

    Send single Slack message with info for all media types on TSV load complete

--- openverse_catalog/dags/common/loader/reporting.py
+++ openverse_catalog/dags/common/loader/reporting.py
@@ -4,7 +4,7 @@ from common.slack import send_message
 
 
 logger = logging.getLogger(__name__)
-
+XCOM_PULL_TEMPLATE = "{{{{ ti.xcom_pull(task_ids='{}', key='{}') }}}}"
 
 # Shamelessly lifted from:
 # https://gist.github.com/borgstrom/936ca741e885a1438c374824efb038b3
@@ -28,7 +28,7 @@ def humanize_time_duration(seconds):
     return ", ".join(parts)
 
 
-def report_completion(provider_name, media_type, duration, record_count):
+def report_completion(provider_name, load_data_stats_by_media_type):
     """
     Send a Slack notification when the load_data task has completed.
     Messages are only sent out in production and if a Slack connection is defined.
@@ -38,13 +38,26 @@ def report_completion(provider_name, media_type, duration, record_count):
     if isinstance(duration, float):
         duration = humanize_time_duration(duration)
 
-    message = f"""
-*Provider*: `{provider_name}`
-*Media Type*: `{media_type}`
-*Number of Records Upserted*: {record_count}
-*Duration of data pull task*: {duration or "_No data_"}
+    media_type_reports = []
+    for media_type, data in load_data_stats_by_media_type.items():
+        # Truncate the duration value if it's provided
+        if isinstance(data["duration"], float):
+            data["duration"] = round(data["duration"], 2)
 
-* _Duration includes time taken to pull data of all media types._
-"""
+        # Build the status report for this media type
+        message = f"""
+    *Media Type*: `{media_type}`
+        *Number of records upserted*: {data['record_count']}
+        *Duration of data pull task*: {data['duration']}
+        """
+        media_type_reports.append(message)
+
+    # Collect data into a single message
+    message = (
+        f"\n*Provider*: `{provider_name}`\n"
+        + "".join(media_type_reports)
+        + "\n* _Duration includes time taken to pull data of all media types._"
+    )
+
     send_message(message, username="Airflow DAG Load Data Complete")
     logger.info(message)
