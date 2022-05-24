954593b61f32507ff9a82ec401d803c71819b416
Olga Bulat <obulat@gmail.com>
2022-05-24 06:22:18 +0300

    Use function to compute license map
    Signed-off-by: Olga Bulat <obulat@gmail.com>

--- openverse_catalog/dags/common/add_license_url.py
+++ openverse_catalog/dags/common/add_license_url.py
@@ -2,6 +2,7 @@ import logging
 from textwrap import dedent
 
 from airflow.providers.postgres.hooks.postgres import PostgresHook
+from common.licenses.constants import get_reverse_license_path_map
 
 
 RETURN_ROW_COUNT = lambda c: c.rowcount  # noqa: E731
@@ -10,61 +11,7 @@ logger = logging.getLogger(__name__)
 
 base_url = "https://creativecommons.org/"
 
-license_map = {
-    ("by", "1.0"): "licenses/by/1.0",
-    ("by", "2.0"): "licenses/by/2.0",
-    ("by", "2.5"): "licenses/by/2.5",
-    ("by", "3.0"): "licenses/by/3.0",
-    ("by", "4.0"): "licenses/by/4.0",
-    ("by-nc", "1.0"): "licenses/by-nc/1.0",
-    ("by-nc", "2.0"): "licenses/by-nc/2.0",
-    ("by-nc", "2.5"): "licenses/by-nc/2.5",
-    ("by-nc", "3.0"): "licenses/by-nc/3.0",
-    ("by-nc", "4.0"): "licenses/by-nc/4.0",
-    ("by-nc-nd", "2.0"): "licenses/by-nc-nd/2.0",
-    ("by-nc-nd", "2.5"): "licenses/by-nc-nd/2.5",
-    ("by-nc-nd", "3.0"): "licenses/by-nc-nd/3.0",
-    ("by-nc-nd", "4.0"): "licenses/by-nc-nd/4.0",
-    ("by-nc-sa", "1.0"): "licenses/by-nc-sa/1.0",
-    ("by-nc-sa", "2.0"): "licenses/by-nc-sa/2.0",
-    ("by-nc-sa", "2.5"): "licenses/by-nc-sa/2.5",
-    ("by-nc-sa", "3.0"): "licenses/by-nc-sa/3.0",
-    ("by-nc-sa", "4.0"): "licenses/by-nc-sa/4.0",
-    ("by-nd", "1.0"): "licenses/by-nd/1.0",
-    ("by-nd", "2.0"): "licenses/by-nd/2.0",
-    ("by-nd", "2.5"): "licenses/by-nd/2.5",
-    ("by-nd", "3.0"): "licenses/by-nd/3.0",
-    ("by-nd", "4.0"): "licenses/by-nd/4.0",
-    ("by-sa", "1.0"): "licenses/by-sa/1.0",
-    ("by-sa", "2.0"): "licenses/by-sa/2.0",
-    ("by-sa", "2.5"): "licenses/by-sa/2.5",
-    ("by-sa", "3.0"): "licenses/by-sa/3.0",
-    ("by-sa", "4.0"): "licenses/by-sa/4.0",
-    ("devnations", "2.0"): "licenses/devnations/2.0",
-    ("nc", "1.0"): "licenses/nc/1.0",
-    ("nc", "2.0"): "licenses/nc/2.0/jp",
-    ("nc-sa", "1.0"): "licenses/nc-sa/1.0",
-    ("nc-sa", "2.0"): "licenses/nc-sa/2.0/jp",
-    ("nc-sampling+", "1.0"): "licenses/nc-sampling+/1.0",
-    ("nd", "1.0"): "licenses/nd/1.0",
-    ("nd", "2.0"): "licenses/nd/2.0/jp",
-    ("nd-nc", "1.0"): "licenses/nd-nc/1.0",
-    ("nd-nc", "2.0"): "licenses/nd-nc/2.0/jp",
-    ("sa", "1.0"): "licenses/sa/1.0",
-    ("sa", "2.0"): "licenses/sa/2.0/jp",
-    ("sampling+", "1.0"): "licenses/sampling+/1.0",
-    ("sampling", "1.0"): "licenses/sampling/1.0",
-    ("by-nc-nd", "1.0"): "licenses/by-nd-nc/1.0",
-    ("publicdomain", "N/A"): "licenses/publicdomain",
-    ("pdm", "1.0"): "publicdomain/mark/1.0",
-    ("cc0", "1.0"): "publicdomain/zero/1.0",
-    ("by", "2.1"): "licenses/by/2.0",
-    ("by-nc", "2.1"): "licenses/by-nc/2.0",
-    ("by-nc-nd", "2.1"): "licenses/by-nc-nd/2.0",
-    ("by-nc-sa", "2.1"): "licenses/by-nc-sa/2.0",
-    ("by-nd", "2.1"): "licenses/by-nd/2.0",
-    ("by-sa", "2.1"): "licenses/by-sa/2.0",
-}
+license_map = get_reverse_license_path_map()
 
 
 def make_sample_data(postgres_conn_id: str):
