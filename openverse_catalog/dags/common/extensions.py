75a372c24cddc032de2dee57123d0f12664bc2a4
Olga Bulat <obulat@gmail.com>
2022-06-02 16:12:09 +0300

    Add extract_filetype utility function
    Signed-off-by: Olga Bulat <obulat@gmail.com>

---
+++ openverse_catalog/dags/common/extensions.py
@@ -0,0 +1,18 @@
+from typing import Literal
+
+
+EXTENSIONS = {
+    "image": ["jpg", "jpeg", "png", "gif", "bmp", "webp", "tiff", "tif", "svg", "webp"],
+    "audio": ["mp3", "ogg", "wav", "aiff", "flac", "aac", "m4a", "wma", "mp4", "m4b"],
+}
+
+
+def extract_filetype(url: str, media_type: Literal["image", "audio"]) -> str | None:
+    """
+    Extracts the filetype from a media url extension.
+    """
+    possible_filetype = url.split(".")[-1]
+    if possible_filetype in EXTENSIONS[media_type]:
+        return possible_filetype
+    else:
+        return None
