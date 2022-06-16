c4ad5278a26a764ba696a053643c640447c562c4
Staci Cooper <staci.cooper@automattic.com>
2022-06-16 14:07:15 -0700

    Update WorkflowDagFactory to pass the ingester class if it exists

--- openverse_catalog/dags/providers/provider_workflow_dag_factory.py
+++ openverse_catalog/dags/providers/provider_workflow_dag_factory.py
@@ -16,9 +16,12 @@ for provider_workflow in PROVIDER_WORKFLOWS:
         f"providers.provider_api_scripts.{provider_workflow.provider_script}"
     )
 
+    # Use the ingester class if it exists, or fall back to the `main` method
+    ingestion_callable = provider_workflow.ingester_class or provider_script.main
+
     globals()[provider_workflow.dag_id] = create_provider_api_workflow(
         provider_workflow.dag_id,
-        provider_script.main,
+        ingestion_callable,
         provider_workflow.default_args,
         provider_workflow.start_date,
         provider_workflow.max_active_runs,
