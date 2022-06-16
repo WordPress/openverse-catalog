4247acad2b378dccc1c5298c7f5e714e9353f19b
Staci Cooper <staci.cooper@automattic.com>
2022-06-16 14:11:55 -0700

    Update Cleveland Museum to use ProviderDataIngester

--- openverse_catalog/dags/providers/provider_api_scripts/cleveland_museum.py
+++ openverse_catalog/dags/providers/provider_api_scripts/cleveland_museum.py
@@ -1,161 +1,112 @@
 import logging
-from typing import Dict, List
+from typing import Dict, List, Optional
 
 from common.licenses import get_license_info
 from common.loader import provider_details as prov
-from common.requester import DelayedRequester
-from common.storage.image import ImageStore
+from providers.provider_api_scripts.provider_data_ingester import ProviderDataIngester
 
 
-LIMIT = 1000
-DELAY = 5.0
-RETRIES = 3
-PROVIDER = prov.CLEVELAND_DEFAULT_PROVIDER
-ENDPOINT = "http://openaccess-api.clevelandart.org/api/artworks/"
-CC0_LICENSE = get_license_info(license_="cc0", license_version="1.0")
-
-delay_request = DelayedRequester(delay=DELAY)
-image_store = ImageStore(provider=PROVIDER)
-
-DEFAULT_QUERY_PARAMS = {"cc": "1", "has_image": "1", "limit": LIMIT, "skip": 0}
-
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s:  %(message)s", level=logging.INFO
-)
 logger = logging.getLogger(__name__)
 
+CC0_LICENSE = get_license_info(license_="cc0", license_version="1.0")
 
-def main():
-    logger.info("Begin: Cleveland Museum API requests")
-    condition = True
-    offset = 0
 
-    while condition:
-        query_param = _build_query_param(offset)
-        response_json, total_images = _get_response(query_param)
-        if response_json is not None and total_images != 0:
-            batch = response_json["data"]
-            image_count = _handle_response(batch)
-            logger.info(f"Total images till now {image_count}")
-            offset += LIMIT
+class ClevelandDataIngester(ProviderDataIngester):
+    def __init__(
+        self,
+        providers={"image": prov.CLEVELAND_DEFAULT_PROVIDER},
+        endpoint="http://openaccess-api.clevelandart.org/api/artworks/",
+        delay=5,
+        batch_limit=1000,
+        retries=3,
+        headers: Optional[Dict] = {},
+    ):
+        # Initialize the DataIngester with appropriate values
+        super().__init__(providers, endpoint, delay, batch_limit, retries, headers)
+
+    def get_next_query_params(self, old_query_params, **kwargs):
+        if not old_query_params:
+            # Return default query params on the first request
+            return {"cc": "1", "has_image": "1", "limit": self.batch_limit, "skip": 0}
         else:
-            logger.error("No more images to process")
-            logger.info("Exiting")
-            condition = False
-    image_count = image_store.commit()
-    logger.info(f"Total number of images received {image_count}")
+            # Increment `skip` by the batch limit.
+            return {
+                **old_query_params,
+                "skip": old_query_params["skip"] + self.batch_limit,
+            }
 
+    def get_media_type(self, record):
+        # This provider only supports Images.
+        return "image"
 
-def _build_query_param(offset=0, default_query_param=None):
-    if default_query_param is None:
-        default_query_param = DEFAULT_QUERY_PARAMS
-    query_param = default_query_param.copy()
-    query_param.update(skip=offset)
-    return query_param
+    def get_record_data(self, record):
+        license_ = record.get("share_license_status", "").lower()
+        if license_ != "cc0":
+            logger.error("Wrong license image")
+            return None
 
+        foreign_id = record.get("id")
+        if foreign_id is None:
+            return None
 
-def _get_response(query_param, endpoint=ENDPOINT, retries=RETRIES):
-    response_json, total_images, tries = None, 0, 0
-    for tries in range(retries):
-        response = delay_request.get(endpoint, query_param)
-        if response is not None and response.status_code == 200:
-            try:
-                response_json = response.json()
-                total_images = len(response_json["data"])
-            except Exception as e:
-                logger.warning(f"response not captured due to {e}")
-                response_json = None
-            if response_json is not None and total_images is not None:
-                break
+        image = self._get_image_type(record.get("images", {}))
+        if image is None or image.get("url") is None:
+            return None
 
-        logger.info(
-            "Retrying \n"
-            f"endpoint -- {endpoint} \t"
-            f" with parameters -- {query_param} "
-        )
-    if tries == retries - 1 and ((response_json is None) or (total_images is None)):
-        logger.warning("No more tries remaining. Returning Nonetypes.")
-        return None, 0
-    else:
-        return response_json, total_images
+        if record.get("creators"):
+            creator_name = record.get("creators")[0].get("description", "")
+        else:
+            creator_name = ""
+        
+        return {
+            "foreign_identifier": f"{foreign_id}",
+            "foreign_landing_url": record.get("url"),
+            "title": record.get("title", None),
+            "creator": creator_name,
+            "image_url": image["url"],
+            "width": self._get_int_value(image, "width"),
+            "height": self._get_int_value(image, "height"),
+            "filesize": self._get_int_value(image, "filesize"),
+            "license_info": CC0_LICENSE,
+            "meta_data": self._get_metadata(record),
+        }
 
+    def _get_image_type(self, image_data):
+        key, image_url = None, None
+        if image_data.get("web"):
+            key = "web"
+            image_url = image_data.get("web").get("url", None)
+        elif image_data.get("print"):
+            key = "print"
+            image_url = image_data.get("print").get("url", None)
+        elif image_data.get("full"):
+            key = "full"
+            image_url = image_data.get("full").get("url", None)
+        return image_url, key
 
-def get_int_value(data: Dict, key: str) -> int | None:
-    """
-    Converts the value of the key `key` in `data` to an integer.
-    Returns None if the value is not convertible to an integer, or
-    if the value doesn't exist.
-    """
-    value = data.get(key)
-    if bool(value):
-        if isinstance(value, str) and value.isdigit():
-            return int(value)
-        elif isinstance(value, int):
-            return value
-    return None
-
-
-def _handle_batch_item(data: Dict) -> Dict | None:
-    license_ = data.get("share_license_status", "").lower()
-    if license_ != "cc0":
+    def _get_int_value(self, data: Dict, key: str) -> int | None:
+        """
+        Converts the value of the key `key` in `data` to an integer.
+        Returns None if the value is not convertible to an integer, or
+        if the value doesn't exist.
+        """
+        value = data.get(key)
+        if bool(value):
+            if isinstance(value, str) and value.isdigit():
+                return int(value)
+            elif isinstance(value, int):
+                return value
         return None
-    foreign_id = data.get("id")
-    if foreign_id is None:
-        return None
-    image = _get_image_type(data.get("images", {}))
-    if image is None or image.get("url") is None:
-        return None
 
-    if data.get("creators"):
-        creator_name = data.get("creators")[0].get("description", "")
-    else:
-        creator_name = ""
-
-    return {
-        "foreign_identifier": f"{foreign_id}",
-        "foreign_landing_url": data.get("url"),
-        "title": data.get("title", None),
-        "creator": creator_name,
-        "image_url": image["url"],
-        "width": get_int_value(image, "width"),
-        "height": get_int_value(image, "height"),
-        "filesize": get_int_value(image, "filesize"),
-        "license_info": CC0_LICENSE,
-        "meta_data": _get_metadata(data),
-    }
-
-
-def _handle_response(batch: List):
-    total_images = 0
-    for data in batch:
-        item = _handle_batch_item(data)
-        if item is not None:
-            total_images = image_store.add_item(**item)
-
-    return total_images
-
-
-def _get_image_type(image_data):
-    # Returns the image url and key for the image in `image_data` dict.
-    for key in ["web", "print", "full"]:
-        if keyed_image := image_data.get(key):
-            return keyed_image
-    return None
-
-
-def _get_metadata(data):
-    metadata = {
-        "accession_number": data.get("accession_number", ""),
-        "technique": data.get("technique", ""),
-        "date": data.get("creation_date", ""),
-        "credit_line": data.get("creditline", ""),
-        "classification": data.get("type", ""),
-        "tombstone": data.get("tombstone", ""),
-        "culture": ",".join([i for i in data.get("culture", []) if i is not None]),
-    }
-    metadata = {k: v for k, v in metadata.items() if v is not None}
-    return metadata
-
-
-if __name__ == "__main__":
-    main()
+    def _get_metadata(self, data):
+        metadata = {
+            "accession_number": data.get("accession_number", ""),
+            "technique": data.get("technique", ""),
+            "date": data.get("creation_date", ""),
+            "credit_line": data.get("creditline", ""),
+            "classification": data.get("type", ""),
+            "tombstone": data.get("tombstone", ""),
+            "culture": ",".join([i for i in data.get("culture", []) if i is not None]),
+        }
+        metadata = {k: v for k, v in metadata.items() if v is not None}
+        return metadata
--- tests/dags/providers/provider_api_scripts/test_cleveland_museum.py
+++ tests/dags/providers/provider_api_scripts/test_cleveland_museum.py
@@ -1,13 +1,15 @@
 import json
 import logging
 from pathlib import Path
+import unittest
 from unittest.mock import MagicMock, patch
 
 import pytest
 import requests
 from common.licenses import LicenseInfo
+from common.loader import provider_details as prov
 from common.storage.image import ImageStore
-from providers.provider_api_scripts import cleveland_museum as clm
+from providers.provider_api_scripts.cleveland_museum import ClevelandDataIngester
 
 
 @pytest.fixture(autouse=True)
@@ -24,9 +26,7 @@ _license_info = (
     None,
 )
 CC0_LICENSE = LicenseInfo(*_license_info)
-clm.image_store = ImageStore(
-    provider=clm.PROVIDER,
-)
+license_info = LicenseInfo(*_license_info)
 
 RESOURCES = Path(__file__).parent / "resources/clevelandmuseum"
 
@@ -35,197 +35,138 @@ logging.basicConfig(
 )
 
 
-def _get_resource_json(json_name):
-    with open(RESOURCES / json_name) as f:
-        resource_json = json.load(f)
-        return resource_json
+class TestClevelandDataIngester(unittest.TestCase):
+    def setUp(self):
+        self.clm = ClevelandDataIngester()
+        self.image_store = ImageStore(
+            provider=prov.CLEVELAND_DEFAULT_PROVIDER
+        )
+        self.clm.media_stores = {
+            "image": self.image_store
+        }
 
+    def _get_resource_json(self, json_name):
+        with open(RESOURCES / json_name) as f:
+            resource_json = json.load(f)
+            return resource_json
 
-def test_build_query_param_default():
-    actual_param = clm._build_query_param()
-    expected_param = {"cc": "1", "has_image": "1", "limit": 1000, "skip": 0}
-    assert actual_param == expected_param
+    def test_build_query_param_default(self):
+        actual_param = self.clm.get_next_query_params({})
+        expected_param = {"cc": "1", "has_image": "1", "limit": 1000, "skip": 0}
+        assert actual_param == expected_param
 
+    def test_build_query_param_increments_offset(self):
+        previous_query_params = {"cc": "1", "has_image": "1", "limit": 1000, "skip": 0}
 
-def test_build_query_param_with_givens():
-    actual_param = clm._build_query_param(offset=1000)
-    expected_param = {"cc": "1", "has_image": "1", "limit": 1000, "skip": 1000}
-    assert actual_param == expected_param
+        actual_param = self.clm.get_next_query_params(previous_query_params)
+        expected_param = {"cc": "1", "has_image": "1", "limit": 1000, "skip": 1000}
+        assert actual_param == expected_param
 
+    def test_get_image_type_web(self):
+        image_data = self._get_resource_json("image_type_web.json")
+        actual_url, actual_key = self.clm._get_image_type(image_data)
 
-def test_get_image_type_web():
-    response_json = _get_resource_json("response_success.json")
-    image_data = _get_resource_json("image_type_web.json")
-    data = response_json["data"][0]
-    data["images"] = image_data
-    with patch.object(clm.image_store, "save_item") as mock_save_item:
-        clm._handle_response([data])
+        expected_url = "https://openaccess-cdn.clevelandart.org/1335.1917/1335.1917_web.jpg"
+        expected_key = "web"
 
-    expected_image = {
-        "url": "https://openaccess-cdn.clevelandart.org/1335.1917/1335.1917_web.jpg",
-        "width": 1263,
-        "height": 775,
-        "filesize": 716717,
-    }
+        assert actual_url == expected_url
+        assert actual_key == expected_key
 
-    actual_image = mock_save_item.call_args[0][0]
-    for key, value in expected_image.items():
-        assert getattr(actual_image, key) == value
+    def test_get_image_type_print(self):
+        image_data = self._get_resource_json("image_type_print.json")
+        actual_url, actual_key = self.clm._get_image_type(image_data)
+        expected_url = (
+            "https://openaccess-cdn.clevelandart.org/" "1335.1917/1335.1917_print.jpg"
+        )
+        expected_key = "print"
 
+        assert actual_url == expected_url
+        assert actual_key == expected_key
 
-def test_get_image_type_print():
-    response_json = _get_resource_json("response_success.json")
-    image_data = _get_resource_json("image_type_print.json")
-    data = response_json["data"][0]
-    data["images"] = image_data
-    with patch.object(clm.image_store, "save_item") as mock_save_item:
-        clm._handle_response([data])
+    def test_get_image_type_full(self):
+        image_data = self._get_resource_json("image_type_full.json")
+        actual_url, actual_key = self.clm._get_image_type(image_data)
+        expected_url = (
+            "https://openaccess-cdn.clevelandart.org/" "1335.1917/1335.1917_full.tif"
+        )
+        expected_key = "full"
 
-    expected_image = {
-        "url": "https://openaccess-cdn.clevelandart.org/1335.1917/1335.1917_print.jpg",
-        "width": 3400,
-        "height": 2086,
-        "filesize": 5582485,
-    }
+        assert actual_url == expected_url
+        assert actual_key == expected_key
 
-    actual_image = mock_save_item.call_args[0][0]
-    for key, value in expected_image.items():
-        assert getattr(actual_image, key) == value
+    def test_get_image_type_none(self):
+        image_data = self._get_resource_json("image_type_none.json")
+        actual_url, actual_key = self.clm._get_image_type(image_data)
+        expected_url = None
+        expected_key = None
 
+        assert actual_url == expected_url
+        assert actual_key == expected_key
 
-def test_get_image_type_full():
-    response_json = _get_resource_json("response_success.json")
-    image_data = _get_resource_json("image_type_full.json")
-    data = response_json["data"][0]
-    data["images"] = image_data
-    batch = [data]
-    with patch.object(clm.image_store, "save_item") as mock_save_item:
-        clm._handle_response(batch)
-    actual_image = mock_save_item.call_args[0][0]
+    def test_get_metadata(self):
+        data = self._get_resource_json("complete_data.json")
+        actual_metadata = self.clm._get_metadata(data)
+        expected_metadata = self._get_resource_json("expect_metadata.json")
+        assert actual_metadata == expected_metadata
 
-    expected_image = {
-        "url": "https://openaccess-cdn.clevelandart.org/1335.1917/1335.1917_full.tif",
-        "width": 6280,
-        "height": 3853,
-        "filesize": 72628688,
-        "filetype": "tiff",
-    }
+    def test_get_response_success(self):
+        query_param = {"cc": 1, "has_image": 1, "limit": 1, "skip": 30000}
+        response_json = self._get_resource_json("response_success.json")
+        r = requests.Response()
+        r.status_code = 200
+        r.json = MagicMock(return_value=response_json)
+        with patch.object(
+            self.clm.delayed_requester, "get", return_value=r
+        ) as mock_get:
+            batch, _ = self.clm.get_batch(query_param)
+        expected_response = self._get_resource_json("response_success.json")
 
-    for key, value in expected_image.items():
-        assert getattr(actual_image, key) == value
+        assert mock_get.call_count == 1
+        assert response_json == expected_response
+        assert len(batch) == 1
 
+    def test_get_response_no_data(self):
+        query_param = {"cc": 1, "has_image": 1, "limit": 1, "skip": 33000}
+        response_json = self._get_resource_json("response_no_data.json")
+        r = requests.Response()
+        r.status_code = 200
+        r.json = MagicMock(return_value=response_json)
+        with patch.object(
+            self.clm.delayed_requester, "get", return_value=r
+        ) as mock_get:
+            batch, should_continue = self.clm.get_batch(query_param)
+        expected_response = self._get_resource_json("response_no_data.json")
 
-def test_get_image_type_none():
-    response_json = _get_resource_json("response_success.json")
-    image_data = _get_resource_json("image_type_none.json")
-    data = response_json["data"][0]
-    data["images"] = image_data
-    actual_image = clm._handle_batch_item(data)
+        assert mock_get.call_count == 1
+        assert response_json == expected_response
+        assert len(batch) == 0
 
-    assert actual_image is None
+    def test_get_response_failure(self):
+        query_param = {"cc": 1, "has_image": 1, "limit": 1, "skip": -1}
+        r = requests.Response()
+        r.status_code = 500
+        r.json = MagicMock(return_value={"error": ""})
+        with patch.object(
+            self.clm.delayed_requester, "get", return_value=r
+        ) as mock_get:
+            self.clm.get_batch(query_param)
 
+        assert mock_get.call_count == 4
 
-def test_get_metadata():
-    data = _get_resource_json("complete_data.json")
-    actual_metadata = clm._get_metadata(data)
-    expected_metadata = _get_resource_json("expect_metadata.json")
-    assert actual_metadata == expected_metadata
+    def test_get_response_None(self):
+        query_param = {"cc": 1, "has_image": 1, "limit": 1, "skip": -1}
+        with patch.object(
+            self.clm.delayed_requester, "get", return_value=None
+        ) as mock_get:
+            batch, _ = self.clm.get_batch(query_param)
 
+        assert batch is None
+        assert mock_get.call_count == 4
 
-def test_get_response_success():
-    query_param = {"cc": 1, "has_image": 1, "limit": 1, "skip": 30000}
-    response_json = _get_resource_json("response_success.json")
-    r = requests.Response()
-    r.status_code = 200
-    r.json = MagicMock(return_value=response_json)
-    with patch.object(clm.delay_request, "get", return_value=r) as mock_get:
-        response_json, total_images = clm._get_response(query_param)
-    expected_response = _get_resource_json("response_success.json")
+    def test_handle_response(self):
+        response_json = self._get_resource_json("handle_response_data.json")
+        data = response_json["data"]
+        actual_total_images = self.clm.process_batch(data)
+        expected_total_images = 100
 
-    assert mock_get.call_count == 1
-    assert response_json == expected_response
-    assert total_images == 1
-
-
-def test_get_response_no_data():
-    query_param = {"cc": 1, "has_image": 1, "limit": 1, "skip": 33000}
-    response_json = _get_resource_json("response_no_data.json")
-    r = requests.Response()
-    r.status_code = 200
-    r.json = MagicMock(return_value=response_json)
-    with patch.object(clm.delay_request, "get", return_value=r) as mock_get:
-        response_json, total_images = clm._get_response(query_param)
-    expected_response = _get_resource_json("response_no_data.json")
-
-    assert mock_get.call_count == 1
-    assert response_json == expected_response
-    assert total_images == 0
-
-
-def test_get_response_failure():
-    query_param = {"cc": 1, "has_image": 1, "limit": 1, "skip": -1}
-    r = requests.Response()
-    r.status_code = 500
-    r.json = None
-    with patch.object(clm.delay_request, "get", return_value=r) as mock_get:
-        clm._get_response(query_param)
-
-    assert mock_get.call_count == 3
-
-
-def test_handle_single_response():
-    response_json = _get_resource_json("response_success.json")
-    batch = response_json["data"]
-    # Patching save_item because it will have the filetype after clean_metadata
-    with patch.object(clm.image_store, "save_item") as mock_save_item:
-        clm._handle_response(batch)
-    expected_image = {
-        "creator": "",
-        "foreign_identifier": "96887",
-        "foreign_landing_url": "https://clevelandart.org/art/1916.586.a",
-        "width": 641,
-        "height": 900,
-        "filesize": 222248,
-        "filetype": "jpg",
-        "url": "https://openaccess-cdn.clevelandart.org/1916.586.a/1916.586.a_web.jpg",
-        "license_": CC0_LICENSE.license,
-        "license_version": CC0_LICENSE.version,
-        "meta_data": {
-            "license_url": CC0_LICENSE.url,
-            "raw_license_url": None,
-            "accession_number": "1916.586.a",
-            "classification": "Miscellaneous",
-            "credit_line": "Gift of Mr. and Mrs. J. H. Wade",
-            "culture": "Germany, 18th century",
-            "date": "1700s",
-            "tombstone": "Scent Bottle, 1700s. Germany, 18th century. Glass with "
-            "enamel decoration; overall: 10.2 cm (4 in.). The Cleveland "
-            "Museum of Art, Gift of Mr. and Mrs. J. H. Wade 1916.586.a",
-            "technique": "glass with enamel decoration",
-        },
-        "title": "Scent Bottle",
-    }
-
-    actual_image = mock_save_item.call_args[0][0]
-    for key, value in expected_image.items():
-        assert getattr(actual_image, key) == value
-
-
-def test_get_response_None():
-    query_param = {"cc": 1, "has_image": 1, "limit": 1, "skip": -1}
-    with patch.object(clm.delay_request, "get", return_value=None) as mock_get:
-        response_json, total_images = clm._get_response(query_param)
-
-    assert response_json is None
-    assert total_images == 0
-    # Retries
-    assert mock_get.call_count == 3
-
-
-def test_handle_response():
-    response_json = _get_resource_json("handle_response_data.json")
-    data = response_json["data"]
-    actual_total_images = clm._handle_response(data)
-    expected_total_images = 100
-
-    assert actual_total_images == expected_total_images
+        assert actual_total_images == expected_total_images
