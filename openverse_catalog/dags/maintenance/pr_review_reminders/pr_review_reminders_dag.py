f638ca7ccd0a94635c5515d2e5a0123b838f6213
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2022-06-23 10:09:46 +1000

    Move files and clean up default dry_run values

--- openverse_catalog/dags/maintenance/pr_review_reminders/pr_review_reminders_dag.py
+++ openverse_catalog/dags/maintenance/pr_review_reminders/pr_review_reminders_dag.py
@@ -29,7 +29,8 @@ from common.constants import DAG_DEFAULT_ARGS
 
 DAG_ID = "pr_review_reminders"
 MAX_ACTIVE_TASKS = 1
-DRY_RUN = Variable.get("PR_REVIEW_REMINDER_DRY_RUN", default_var=True)
+ENVIRONMENT = Variable.get("ENVIRONMENT", default_var="dev")
+DRY_RUN = Variable.get("PR_REVIEW_REMINDER_DRY_RUN", default_var=(ENVIRONMENT == "dev"))
 GITHUB_PAT = Variable.get("GITHUB_API_KEY", default_var="not_set")
 
 dag = DAG(
