8add95796b2bfc8ea795256ae0eb2ba7a3245420
rwidom <rebeccawidom@gmail.com>
2022-07-03 17:59:03 -0400

    the refactored script

--- openverse_catalog/dags/providers/provider_api_scripts/stocksnap.py
+++ openverse_catalog/dags/providers/provider_api_scripts/stocksnap.py
@@ -3,20 +3,25 @@ Content Provider:       StockSnap
 
 ETL Process:            Use the API to identify all CC-licensed images.
 
-Output:                 TSV file containing the image, the respective
-                        meta-data.
+Output:                 TSV file containing the image, the respective meta-data.
 
-Notes:                  https://stocksnap.io/api/
-                        No rate limit specified. No authentication required.
+Notes:                  https://stocksnap.io/faq
                         All images are licensed under CC0.
+                        Seems like their API has been suspended, but this assumes
+                        that there is a special agreement with Openverse.
 """
 import json
 import logging
 
 from common.licenses import get_license_info
 from common.loader import provider_details as prov
-from common.requester import DelayedRequester
-from common.storage.image import ImageStore
+
+# There are a couple of things that seem really broadly applicable from the Cleveland
+# sample script: the method _get_int_value and the constant CC0_LICENSE.
+# I'm not using the latter, because it fails some of the existing StockSnap tests,
+# because of a difference in trailing / in the license url.
+from providers.provider_api_scripts.cleveland_museum import ClevelandDataIngester
+from providers.provider_api_scripts.provider_data_ingester import ProviderDataIngester
 
 
 logging.basicConfig(
@@ -28,175 +33,165 @@ logger = logging.getLogger(__name__)
 DELAY = 1  # in seconds
 RETRIES = 3
 HOST = "stocksnap.io"
-ENDPOINT = f"https://{HOST}/api/load-photos/date/desc"
+ENDPOINT_BASE = f"https://{HOST}/api/load-photos/date/desc"
 IMAGE_CDN = "https://cdn.stocksnap.io/img-thumbs/960w"
 PROVIDER = prov.STOCKSNAP_DEFAULT_PROVIDER
-HEADERS = {
-    "Accept": "application/json",
-}
 
-delayed_requester = DelayedRequester(DELAY)
-image_store = ImageStore(provider=PROVIDER)
 
-license_url = "https://creativecommons.org/publicdomain/zero/1.0/"
-license_info = get_license_info(license_url=license_url)
+class StockSnapDataIngester(ProviderDataIngester):
+    providers = {"image": prov.STOCKSNAP_DEFAULT_PROVIDER}
+    batch_limit = 1000
+    delay = 1  # in seconds
+    headers = {
+        "Accept": "application/json",
+    }
+
+    def __init__(self):
+        super(StockSnapDataIngester, self).__init__()
+        self._page_counter = 1
+
+    def get_next_query_params(self):
+        # StockSnap uses /{page} at the end of the endpoint url instead of query params.
+        # Page number is incremented in get_should_continue below.
+        return None
+
+    def get_media_type(self):
+        return "image"
+
+    def endpoint(self):
+        return f"{ENDPOINT_BASE}/{self._page_counter}"
+
+    def get_should_continue(self, response_json):
+        if isinstance(response_json, list) and len(response_json) > 0:
+            self._page_counter += 1
+            return True
+        else:
+            return False
+
+    def get_batch_data(self, response_json):
+        """
+        Take an API response and return the list of records.
+        """
+        # Based on the sample test file, using json.load on the file will return a list.
+        if isinstance(response_json, list) and len(response_json) > 0:
+            return response_json
+        return None
+
+    def get_record_data(self, data):
+        """
+        Parse out the necessary information (license info, urls, etc) from the record
+        data (single image dict-like thing) into a dictionary.
+        """
+        try:
+            foreign_id = data["img_id"]
+        except (TypeError, KeyError, AttributeError):
+            return None
+
+        foreign_landing_url = f"https://{HOST}/photo/{foreign_id}"
+
+        image_url, width, height = self._get_image_info(data)
+        if image_url is None:
+            logger.info("Found no image url.")
+            logger.info(f"{json.dumps(data, indent=2)}")
+            return None
+
+        title = self._get_title(data)
+        if title is None:
+            logger.info("Found no image title.")
+            logger.info(f"{json.dumps(data, indent=2)}")
+            return None
+
+        creator, creator_url = self._get_creator_data(data)
+        metadata = self._get_metadata(data)
+        tags = self._get_tags(data)
+        filesize = self._get_filesize(image_url)
+        lic = get_license_info("https://creativecommons.org/publicdomain/zero/1.0/")
+
+        return {
+            "title": title,
+            "creator": creator,
+            "creator_url": creator_url,
+            "foreign_identifier": foreign_id,
+            "foreign_landing_url": foreign_landing_url,
+            "image_url": image_url,
+            "filesize": filesize,
+            "filetype": "jpg",
+            "height": height,
+            "width": width,
+            "license_info": lic,
+            "meta_data": metadata,
+            "raw_tags": tags,
+            "category": prov.DEFAULT_IMAGE_CATEGORY[PROVIDER],
+        }
+
+    @staticmethod
+    def _get_image_info(item):
+        width = ClevelandDataIngester._get_int_value(item, "img_width")
+        height = ClevelandDataIngester._get_int_value(item, "img_height")
+        img_id = item.get("img_id")
+        image_url = f"{IMAGE_CDN}/{img_id}.jpg"
+        return image_url, width, height
+
+    @staticmethod
+    def _get_creator_data(item):
+        """
+        Get the author's name and website preferring their custom link over the
+        StockSnap profile. The latter is used if the first is not found.
+        """
+        creator_name = item.get("author_name")
+        if creator_name is None:
+            return None, None
+        creator_url = item.get("author_website")
+        if creator_url is None or creator_url in [
+            "https://stocksnap.io/",
+            "https://stocksnap.io/author/undefined/",
+        ]:
+            creator_url = item.get("author_profile")
+        return creator_name, creator_url
+
+    @staticmethod
+    def _get_title(item):
+        """
+        Get the first two photo's tags/keywords to make the title and transform it
+        to title case, as shown on its page.
+        """
+        tags = item.get("keywords", [])[:2]
+        if len(tags) > 0:
+            tags.append("Photo")
+            img_title = " ".join(tags)
+            return img_title.title()
+
+    def _get_filesize(self, image_url):
+        """
+        Get the size of the image in bytes.
+        """
+        resp = self.delayed_requester.get(image_url)
+        if resp:
+            filesize = int(resp.headers.get("Content-Length", 0))
+            return filesize if filesize != 0 else None
+
+    @staticmethod
+    def _get_metadata(item):
+        """
+        Include popularity statistics.
+        """
+        extras = ["downloads_raw", "page_views_raw", "favorites_raw"]
+        metadata = {}
+        for key in extras:
+            value = item.get(key)
+            if value is not None:
+                metadata[key] = value
+        return metadata
+
+    @staticmethod
+    def _get_tags(item):
+        return item.get("keywords")
 
 
 def main():
-    """
-    This script pulls all the data from the StockSnap and writes it into a
-    .TSV file to be eventually read into our DB.
-    """
-
-    logger.info("Begin: StockSnap script")
-    image_count = _get_items()
-    image_store.commit()
-    logger.info(f"Total images pulled: {image_count}")
-    logger.info("Terminated!")
-
-
-def _get_items():
-    item_count = 0
-    page_number = 1
-    should_continue = True
-    while should_continue:
-        page_endpoint = f"{ENDPOINT}/{page_number}"
-        batch_data = _get_batch_json(endpoint=page_endpoint)
-        if isinstance(batch_data, list) and len(batch_data) > 0:
-            item_count = _process_item_batch(batch_data)
-            page_number += 1
-        else:
-            should_continue = False
-    return item_count
-
-
-def _get_batch_json(
-    endpoint=ENDPOINT, headers=None, retries=RETRIES, query_params=None
-):
-    if headers is None:
-        headers = HEADERS.copy()
-    response_json = delayed_requester.get_response_json(
-        endpoint, retries, query_params, headers=headers
-    )
-    if response_json is None:
-        return None
-    else:
-        data = response_json.get("results")
-        return data
-
-
-def _process_item_batch(items_batch):
-    for item in items_batch:
-        item_meta_data = _extract_item_data(item)
-        if item_meta_data is None:
-            continue
-        image_store.add_item(**item_meta_data)
-    return image_store.total_items
-
-
-def _extract_item_data(media_data):
-    """
-    Extract data for individual image.
-    """
-    try:
-        foreign_id = media_data["img_id"]
-    except (TypeError, KeyError, AttributeError):
-        return None
-    foreign_landing_url = f"https://{HOST}/photo/{foreign_id}"
-    image_url, width, height = _get_image_info(media_data)
-    if image_url is None:
-        logger.info("Found no image url.")
-        logger.info(f"{json.dumps(media_data, indent=2)}")
-        return None
-    title = _get_title(media_data)
-    if title is None:
-        logger.info("Found no image title.")
-        logger.info(f"{json.dumps(media_data, indent=2)}")
-        return None
-    creator, creator_url = _get_creator_data(media_data)
-    metadata = _get_metadata(media_data)
-    tags = _get_tags(media_data)
-
-    return {
-        "title": title,
-        "creator": creator,
-        "creator_url": creator_url,
-        "foreign_identifier": foreign_id,
-        "foreign_landing_url": foreign_landing_url,
-        "image_url": image_url,
-        "filesize": _get_filesize(image_url),
-        "filetype": "jpg",
-        "height": height,
-        "width": width,
-        "license_info": license_info,
-        "meta_data": metadata,
-        "raw_tags": tags,
-        "category": prov.DEFAULT_IMAGE_CATEGORY[PROVIDER],
-    }
-
-
-def _get_image_info(item):
-    width = item.get("img_width")
-    height = item.get("img_height")
-    img_id = item.get("img_id")
-    image_url = f"{IMAGE_CDN}/{img_id}.jpg"
-    return image_url, width, height
-
-
-def _get_creator_data(item):
-    """
-    Get the author's name and website preferring their custom link over the
-    StockSnap profile. The latter is used if the first is not found.
-    """
-    creator_name = item.get("author_name")
-    if creator_name is None:
-        return None, None
-    creator_url = item.get("author_website")
-    if creator_url is None or creator_url in [
-        "https://stocksnap.io/",
-        "https://stocksnap.io/author/undefined/",
-    ]:
-        creator_url = item.get("author_profile")
-    return creator_name, creator_url
-
-
-def _get_title(item):
-    """
-    Get the first two photo's tags/keywords to make the title and transform it
-    to title case, as shown on its page.
-    """
-    tags = item.get("keywords", [])[:2]
-    if len(tags) > 0:
-        tags.append("Photo")
-        img_title = " ".join(tags)
-        return img_title.title()
-
-
-def _get_filesize(image_url):
-    """
-    Get the size of the image in bytes.
-    """
-    resp = delayed_requester.get(image_url)
-    if resp:
-        filesize = int(resp.headers.get("Content-Length", 0))
-        return filesize if filesize != 0 else None
-
-
-def _get_metadata(item):
-    """
-    Include popularity statistics.
-    """
-    extras = ["downloads_raw", "page_views_raw", "favorites_raw"]
-    metadata = {}
-    for key in extras:
-        value = item.get(key)
-        if value is not None:
-            metadata[key] = value
-    return metadata
-
-
-def _get_tags(item):
-    return item.get("keywords")
+    logger.info("Begin: StockSnap data ingestion")
+    ingester = StockSnapDataIngester()
+    ingester.ingest_records()
 
 
 if __name__ == "__main__":
