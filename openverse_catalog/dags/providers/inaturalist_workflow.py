7fd1102cacdd3c890a8f92fe9ee897ac6159bc68
rwidom <rebeccawidom@gmail.com>
2022-07-14 18:08:31 -0400

    Add check file exists on S3 to DAG

--- openverse_catalog/dags/providers/inaturalist_workflow.py
+++ openverse_catalog/dags/providers/inaturalist_workflow.py
@@ -7,10 +7,13 @@ with a modified `get_response_json` that retrieves paginated data from Postgres 
 than calling an API.
 """
 import logging
+import os
 
 # airflow DAG (necessary for Airflow to find this file)
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.providers.amazon.aws.sensors.s3 import S3KeySensor
+from airflow.utils.task_group import TaskGroup
 from common.constants import DAG_DEFAULT_ARGS
 from providers.provider_api_scripts import inaturalist
 
@@ -20,7 +23,11 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+AWS_CONN_ID = os.getenv("AWS_CONN_ID", "test_conn_id")
+
 DAG_ID = "inaturalist_workflow"
+
+SOURCE_FILE_NAMES = ["observations", "observers", "photos", "taxa"]
 
 with DAG(
     DAG_ID,
@@ -31,43 +38,53 @@ with DAG(
     tags=["inaturalist", "provider: image"],
 ) as dag:
 
+    # Eventually, we'll probably want a function here to confirm that the files have
+    # been updated since the last time we loaded them. But for now, just trying to see
+    # if waiting to load the data might help with intermittent "file doesn't exist" in
+    # the test environment.
+    # Also, in theory s3KeySensor can handle a list of keys
+    # (https://airflow.apache.org/docs/apache-airflow-providers-amazon/stable/_api/airflow/providers/amazon/aws/sensors/s3/index.html#airflow.providers.amazon.aws.sensors.s3.S3KeySensor)
+    # but I'm getting weird type errors when I try to use it, so doing this task
+    # group instead for now.
+    with TaskGroup(group_id="check_sources_exist") as check_sources_exist:
+        upstream_task = None
+        for source_name in SOURCE_FILE_NAMES:
+            globals()[source_name + "_exists"] = S3KeySensor(
+                task_id=source_name + "_exists",
+                bucket_key="s3://inaturalist-open-data/" + source_name + ".csv.gz",
+                aws_conn_id=AWS_CONN_ID,
+                poke_interval=5,
+            )
+            if upstream_task is not None:
+                globals()[source_name + "_exists"].set_upstream(upstream_task)
+            upstream_task = globals()[source_name + "_exists"]
+
     create_schema = PythonOperator(
         task_id="create_schema",
         python_callable=inaturalist.create_schema,
     )
 
-    load_photos = PythonOperator(
-        task_id="load_photos",
-        python_callable=inaturalist.load_photos,
-    )
-
-    load_observations = PythonOperator(
-        task_id="load_observations",
-        python_callable=inaturalist.load_observations,
-    )
-
-    load_taxa = PythonOperator(
-        task_id="load_taxa",
-        python_callable=inaturalist.load_taxa,
-    )
-
-    load_observers = PythonOperator(
-        task_id="load_observers",
-        python_callable=inaturalist.load_observers,
-    )
+    # There is a much more elegant and pythonic way to generate the callables
+    # themselves too, but first let's get the basics running
+    load_callables = [
+        inaturalist.load_photos,
+        inaturalist.load_observations,
+        inaturalist.load_observers,
+        inaturalist.load_taxa,
+    ]
+    with TaskGroup(group_id="load_source_files") as load_source_files:
+        upstream_task = None
+        for (source_name, load_callable) in zip(SOURCE_FILE_NAMES, load_callables):
+            globals()["load_" + source_name] = PythonOperator(
+                task_id="load_" + source_name,
+                python_callable=load_callable,
+            )
+        if upstream_task is not None:
+            globals()["load_" + source_name].set_upstream(upstream_task)
+        upstream_task = globals()["load_" + source_name]
 
     ingest_data = PythonOperator(
         task_id="ingest_data", python_callable=inaturalist.ingest_inaturalist_data
     )
 
-    # single thread just for testing, Airflow was running two things at once, and
-    # seemed to be stuck in a never ending loop of retrying things that had already
-    # finished.
-    (
-        create_schema
-        >> load_photos
-        >> load_observations
-        >> load_observers
-        >> load_taxa
-        >> ingest_data
-    )
+    (check_sources_exist >> create_schema >> load_source_files >> ingest_data)
