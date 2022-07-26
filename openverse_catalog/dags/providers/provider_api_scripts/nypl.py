b094ba90bf8e0c5c7733775a3f9528e2a11e78de
Olga Bulat <obulat@gmail.com>
2022-07-26 10:22:25 +0300

    Refactor NYPL script to use DataIngester class
    Signed-off-by: Olga Bulat <obulat@gmail.com>

--- openverse_catalog/dags/providers/provider_api_scripts/nypl.py
+++ openverse_catalog/dags/providers/provider_api_scripts/nypl.py
@@ -1,12 +1,12 @@
 import logging
+import re
 from urllib.parse import parse_qs, urlparse
 
 from airflow.models import Variable
 from common.licenses import get_license_info
 from common.loader import provider_details as prov
-from common.requester import DelayedRequester
-from common.storage.image import ImageStore
-from requests.exceptions import JSONDecodeError
+from common.loader.provider_details import ImageCategory
+from providers.provider_api_scripts.provider_data_ingester import ProviderDataIngester
 
 
 logging.basicConfig(
@@ -14,198 +14,215 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-LIMIT = 500
-DELAY = 1.0
-RETRIES = 3
-PROVIDER = prov.NYPL_DEFAULT_PROVIDER
-BASE_ENDPOINT = "http://api.repo.nypl.org/api/v1/items/search"
-METADATA_ENDPOINT = "http://api.repo.nypl.org/api/v1/items/item_details/"
 NYPL_API = Variable.get("API_KEY_NYPL", default_var=None)
-TOKEN = f"Token token={NYPL_API}"
+FILETYPE_PATTERN = r" .(jpeg|gif) "
 
-delay_request = DelayedRequester(delay=DELAY)
-image_store = ImageStore(provider=PROVIDER)
 
-DEFAULT_QUERY_PARAMS = {
-    "q": "CC_0",
-    "field": "use_rtxt_s",
-    "page": 1,
-    "per_page": LIMIT,
-}
+class NyplDataIngester(ProviderDataIngester):
+    providers = {"image": prov.NYPL_DEFAULT_PROVIDER}
+    headers = {"Authorization": f"Token token={NYPL_API}"}
+    endpoint = "http://api.repo.nypl.org/api/v1/items/search"
+    metadata_endpoint = "http://api.repo.nypl.org/api/v1/items/item_details/"
+    batch_limit = 500
+    delay = 5
+    image_url_dimensions = ["g", "v", "q", "w", "r"]
 
-HEADERS = {"Authorization": TOKEN}
+    def get_next_query_params(self, prev_query_params, **kwargs):
+        if not prev_query_params:
+            return {
+                "q": "CC_0",
+                "field": "use_rtxt_s",
+                "page": 1,
+                "per_page": self.batch_limit,
+            }
 
-IMAGE_URL_DIMENSIONS = ["g", "v", "q", "w", "r"]
+        else:
+            # Increment `skip` by the batch limit.
+            return {
+                **prev_query_params,
+                "page": prev_query_params["page"] + 1,
+            }
+
+    def get_media_type(self, record):
+        # This provider only supports Images.
+        return "image"
+
+    def get_batch_data(self, response_json):
+        if response_json:
+            return response_json.get("nyplAPI", {}).get("response", {}).get("result")
+        return None
+
+    def get_record_data(self, data):
+        uuid = data.get("uuid")
+
+        item_json = self.get_detail_json(endpoint=self.metadata_endpoint + uuid) or {}
+        item_details = item_json.get("nyplAPI", {}).get("response")
+        if not item_details:
+            return None
+        mods = item_details.get("mods")
+
+        title_info = mods.get("titleInfo")
+        if isinstance(title_info, list) and len(title_info) > 0:
+            title_info = title_info[0]
+        title = title_info.get("title", {}).get("$")
+
+        name_properties = mods.get("name")
+        creator = self._get_creators(name_properties) if name_properties else None
+
+        metadata = self._get_metadata(mods)
+        category = (
+            ImageCategory.PHOTOGRAPH.value
+            if metadata.get("genre") == "Photographs"
+            else None
+        )
+
+        captures = item_details.get("sibling_captures", {}).get("capture")
+        if not captures:
+            return None
+        if not isinstance(captures, list):
+            captures = [captures]
+
+        for img in captures:
+            image_id = img.get("imageID", {}).get("$")
+            if image_id is None:
+                continue
+            image_link = img.get("imageLinks", {}).get("imageLink", [])
+            image_url, filetype = NyplDataIngester._get_image_data(image_link)
+            if not image_url:
+                continue
+            foreign_landing_url = img.get("itemLink", {}).get("$")
+            license_url = img.get("rightsStatementURI", {}).get("$")
+            if not foreign_landing_url or license_url is None:
+                continue
+
+            image_data = {
+                "foreign_identifier": image_id,
+                "foreign_landing_url": foreign_landing_url,
+                "image_url": image_url,
+                "license_info": get_license_info(license_url=license_url),
+                "title": title,
+                "creator": creator,
+                "filetype": filetype,
+                "category": category,
+                "meta_data": metadata,
+            }
+            return image_data
+
+    @staticmethod
+    def _get_filetype(description: str):
+        """
+        Extracts the filetype from a description string like:
+        "Cropped .jpeg (1600 pixels on the long side)"
+        :param description: the description string
+        :return:  jpeg | gif
+        """
+        if match := re.search(FILETYPE_PATTERN, description):
+            return match.group(1)
+        return None
+
+    @staticmethod
+    def _get_image_data(images):
+        """
+        Gets a list of dictionaries of the following shape:
+        {
+          "$": "http://images.nypl.org/index.php?id=56738467&t=q&download=1
+        &suffix=29eed1f0-3d50-0134-c4c7-00505686a51c.001",
+          "description": "Cropped .jpeg (1600 pixels on the long side)"
+        }
+        Extracts the largest image based on the `t` query parameter
+        and IMAGE_URL_DIMENSIONS.
+        """
+
+        image_type = {
+            parse_qs(urlparse(img["$"]).query)["t"][0]: {
+                "url": img["$"],
+                "description": img["description"],
+            }
+            for img in images
+        }
+        if image_type == {}:
+            return None, None
+        preferred_image = (
+            (
+                image_type[dimension]["url"].replace("&download=1", ""),
+                NyplDataIngester._get_filetype(image_type[dimension]["description"]),
+            )
+            for dimension in NyplDataIngester.image_url_dimensions
+            if dimension in image_type
+        )
+        image_url, filetype = next(preferred_image, None)
+        image_url = image_url.replace("http://", "https://")
+        return image_url, filetype
+
+    @staticmethod
+    def _get_creators(creatorinfo):
+        if isinstance(creatorinfo, list):
+            primary_creator = (
+                info.get("namePart", {}).get("$")
+                for info in creatorinfo
+                if info.get("usage") == "primary"
+            )
+            creator = next(primary_creator, None)
+        else:
+            creator = creatorinfo.get("namePart", {}).get("$")
+
+        return creator
+
+    @staticmethod
+    def _get_metadata(mods):
+        metadata = {}
+
+        type_of_resource = mods.get("typeOfResource")
+        if isinstance(type_of_resource, list) and (
+            type_of_resource[0].get("usage") == "primary"
+        ):
+            metadata["type_of_resource"] = type_of_resource[0].get("$")
+
+        if isinstance(mods.get("genre"), dict):
+            metadata["genre"] = mods.get("genre").get("$")
+
+        origin_info = mods.get("originInfo", {})
+        if date_issued := origin_info.get("dateIssued", {}).get("$"):
+            metadata["date_issued"] = date_issued
+        if date_created := origin_info.get("dateCreated", {}):
+            if isinstance(date_created, list):
+                # Approximate dates have a start and an end
+                # [{'encoding': 'w3cdtf', 'keyDate': 'yes',
+                # 'point': 'start', 'qualifier': 'approximate', '$': '1990'},
+                # {'encoding': 'w3cdtf', 'point': 'end',
+                # 'qualifier': 'approximate', '$': '1999'}]
+                start_date = (
+                    d.get("$") for d in date_created if d.get("point") == "start"
+                )
+                end_date = (d.get("$") for d in date_created if d.get("point") == "end")
+                if next(start_date) and next(end_date):
+                    date_created = f"{start_date}-{end_date}"
+                else:
+                    date_created = None
+            else:
+                date_created = date_created.get("$")
+            metadata["date_created"] = date_created
+        if publisher := origin_info.get("publisher", {}).get("$"):
+            metadata["publisher"] = publisher
+
+        if description := mods.get("physicalDescription", {}).get("note", {}).get("$"):
+            metadata["physical_description"] = description
+
+        topics = [
+            subject["topic"].get("$")
+            for subject in mods.get("subject", [])
+            if "topic" in subject and subject["topic"].get("$")
+        ]
+        if topics:
+            metadata["topics"] = ", ".join(topics)
+
+        return metadata
 
 
 def main():
-    page = 1
-    condition = True
-    while condition:
-        query_param = _get_query_param(page=page)
-        request_response = _request_handler(params=query_param)
-        results = request_response.get("result")
-        if type(results) == list and len(results) > 0:
-            _handle_results(results)
-            logger.info(f"{image_store.total_items} images till now")
-            page = page + 1
-        else:
-            condition = False
-    image_store.commit()
-    logger.info(f"total images {image_store.total_items}")
-
-
-def _get_query_param(
-    default_query_params=None,
-    page=1,
-):
-    if default_query_params is None:
-        default_query_params = DEFAULT_QUERY_PARAMS
-    query_param = default_query_params.copy()
-    query_param["page"] = page
-    return query_param
-
-
-def _request_handler(
-    endpoint=BASE_ENDPOINT, params=None, headers=None, retries=RETRIES
-):
-    if headers is None:
-        headers = HEADERS.copy()
-    results = None
-    for retry in range(retries):
-        response = delay_request.get(endpoint, params=params, headers=headers)
-        if response.status_code == 200:
-            try:
-                response_json = response.json()
-                response_json = response_json.get("nyplAPI")
-                results = response_json.get("response")
-                break
-
-            except JSONDecodeError as e:
-                logger.warning(f"Request failed due to {e}")
-                results = None
-        else:
-            results = None
-    return results
-
-
-def _handle_results(results):
-    for item in results:
-        uuid = item.get("uuid")
-
-        item_details = _request_handler(
-            endpoint=METADATA_ENDPOINT + uuid,
-        )
-        if item_details is None:
-            continue
-
-        mods = item_details.get("mods")
-        title = _get_title(mods.get("titleInfo"))
-        creator = _get_creators(mods.get("name"))
-        metadata = _get_metadata(mods)
-
-        captures = item_details.get("sibling_captures", {}).get("capture", [])
-        if type(captures) is not list:
-            captures = [captures]
-
-        _get_capture_details(
-            captures=captures, metadata=metadata, creator=creator, title=title
-        )
-
-
-def _get_capture_details(captures=None, metadata=None, creator=None, title=None):
-    if captures is None:
-        captures = []
-    for img in captures:
-        image_id = img.get("imageID", {}).get("$")
-        if image_id is None:
-            continue
-        image_url = _get_image_url(img.get("imageLinks", {}).get("imageLink", []))
-        foreign_landing_url = img.get("itemLink", {}).get("$")
-        license_url = img.get("rightsStatementURI", {}).get("$")
-        if image_url is None or foreign_landing_url is None or license_url is None:
-            continue
-
-        image_store.add_item(
-            foreign_identifier=image_id,
-            foreign_landing_url=foreign_landing_url,
-            image_url=image_url,
-            license_info=get_license_info(license_url=license_url),
-            title=title,
-            creator=creator,
-            meta_data=metadata,
-        )
-
-
-def _get_title(titleinfo):
-    title = None
-    if type(titleinfo) == list and len(titleinfo) > 0:
-        title = titleinfo[0].get("title", {}).get("$")
-    return title
-
-
-def _get_creators(creatorinfo):
-    if type(creatorinfo) == list:
-        primary_creator = (
-            info.get("namePart", {}).get("$")
-            for info in creatorinfo
-            if info.get("usage") == "primary"
-        )
-        creator = next(primary_creator, None)
-    else:
-        creator = None
-
-    if creator is None:
-        logger.warning("No primary creator found")
-
-    return creator
-
-
-def _get_image_url(images, image_url_dimensions=None):
-    if image_url_dimensions is None:
-        image_url_dimensions = IMAGE_URL_DIMENSIONS
-    image_type = {
-        parse_qs(urlparse(img.get("$")).query)["t"][0]: img.get("$") for img in images
-    }
-
-    image_url = _get_preferred_image(image_type, image_url_dimensions)
-
-    return image_url
-
-
-def _get_preferred_image(image_type, dimension_list):
-    preferred_image = (
-        image_type.get(dimension).replace("&download=1", "")
-        for dimension in dimension_list
-        if dimension in image_type
-    )
-
-    return next(preferred_image, None)
-
-
-def _get_metadata(mods):
-    metadata = {}
-
-    type_of_resource = mods.get("typeOfResource")
-    if isinstance(type_of_resource, list) and (
-        type_of_resource[0].get("usage") == "primary"
-    ):
-        metadata["type_of_resource"] = type_of_resource[0].get("$")
-
-    if isinstance(mods.get("genre"), dict):
-        metadata["genre"] = mods.get("genre").get("$")
-
-    origin_info = mods.get("originInfo")
-    if date_issued := origin_info.get("dateIssued", {}).get("$"):
-        metadata["date_issued"] = date_issued
-    if publisher := origin_info.get("publisher", {}).get("$"):
-        metadata["publisher"] = publisher
-
-    physical_description = mods.get("physicalDescription")
-    if description := physical_description.get("note", {}).get("$"):
-        metadata["description"] = description
-
-    return metadata
+    logger.info("Begin: NYPL data ingestion")
+    ingester = NyplDataIngester()
+    ingester.ingest_records()
 
 
 if __name__ == "__main__":
--- openverse_catalog/dags/providers/provider_workflows.py
+++ openverse_catalog/dags/providers/provider_workflows.py
@@ -9,6 +9,7 @@ from providers.provider_api_scripts.finnish_museums import FinnishMuseumsDataIng
 from providers.provider_api_scripts.inaturalist import INaturalistDataIngester
 from providers.provider_api_scripts.metropolitan_museum import MetMuseumDataIngester
 from providers.provider_api_scripts.museum_victoria import VictoriaDataIngester
+from providers.provider_api_scripts.nypl import NyplDataIngester
 from providers.provider_api_scripts.provider_data_ingester import ProviderDataIngester
 from providers.provider_api_scripts.science_museum import ScienceMuseumDataIngester
 from providers.provider_api_scripts.smk import SmkDataIngester
@@ -170,6 +171,7 @@ PROVIDER_WORKFLOWS = [
     ),
     ProviderWorkflow(
         provider_script="nypl",
+        ingester_class=NyplDataIngester,
         start_date=datetime(2020, 1, 1),
     ),
     ProviderWorkflow(
