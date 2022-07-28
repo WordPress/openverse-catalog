8ea107c910865e1de7ea4436e2e07d53320a089e
Madison Swain-Bowden <bowdenm@spu.edu>
2022-07-28 16:35:29 -0700

    Add record reporting function

---
+++ openverse_catalog/dags/data_refresh/record_reporting.py
@@ -0,0 +1,22 @@
+import logging
+
+from common import slack
+
+
+log = logging.getLogger(__name__)
+
+
+def report_record_difference(before: int, after: int, media_type: str):
+    count_diff = after - before
+    percent_diff = (count_diff / before) * 100
+    # Note for formatting:
+    # '+' - number will always have a sign in front of it
+    # ',' - number is comma separated
+    # '.' - number is a float
+    message = f"""
+**Record count difference for `{media_type}`**: ~{before:,} → ~{after:,}
+**Change**: ~{count_diff:+,} (~{percent_diff}% Δ)
+"""
+    log.info(message)
+    if slack.should_send_message():
+        slack.send_message(text=message, username="Data refresh record difference")
