1e17479d7cec0c46d60efd6767ffad76b706c22c
Staci Cooper <staci.cooper@automattic.com>
2022-08-02 10:39:48 -0700

    Add notifications configuration check to DAG

--- openverse_catalog/dags/maintenance/check_silenced_dags/check_silenced_dags_dag.py
+++ openverse_catalog/dags/maintenance/check_silenced_dags/check_silenced_dags_dag.py
@@ -50,7 +50,7 @@ dag = DAG(
 )
 
 with dag:
-    PythonOperator(
+    check_alerts = PythonOperator(
         task_id="check_silenced_alert_configuration",
         python_callable=check_silenced_dags.check_configuration,
         op_kwargs={
@@ -58,3 +58,14 @@ with dag:
             "airflow_variable": "silenced_slack_alerts",
         },
     )
+
+    check_notifications = PythonOperator(
+        task_id="check_silenced_notification_configuration",
+        python_callable=check_silenced_dags.check_configuration,
+        op_kwargs={
+            "github_pat": GITHUB_PAT,
+            "airflow_variable": "silenced_slack_notifications",
+        },
+    )
+
+    check_alerts >> check_notifications
