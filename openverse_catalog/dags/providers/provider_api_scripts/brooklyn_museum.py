d80bdb1cc2f7ec1c7e7836aedb63adb20ad0785a
Madison Swain-Bowden <bowdenm@spu.edu>
2022-08-31 15:11:08 -0700

    Prefer more generic args

--- openverse_catalog/dags/providers/provider_api_scripts/brooklyn_museum.py
+++ openverse_catalog/dags/providers/provider_api_scripts/brooklyn_museum.py
@@ -16,8 +16,8 @@ class BrooklynMuseumDataIngester(ProviderDataIngester):
     endpoint = "https://www.brooklynmuseum.org/api/v2/object/"
     batch_limit = 35
 
-    def __init__(self, conf: dict = None, date: str = None):
-        super().__init__(conf, date)
+    def __init__(self, *args):
+        super().__init__(*args)
         self.api_key = Variable.get("API_KEY_BROOKLYN_MUSEUM")
         self.headers = {"api_key": self.api_key}
 
