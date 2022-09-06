de045f30235691e950ebf3f45853e1ce9a191e38
rwidom <rebeccawidom@gmail.com>
2022-09-06 18:28:31 -0400

    linting

--- tests/dags/providers/test_on_failure_callback.py
+++ tests/dags/providers/test_on_failure_callback.py
@@ -1,30 +1,31 @@
 import logging
-import psycopg2
-import pytest
+import os
 
-from common.constants import POSTGRES_CONN_ID
-from common.on_failure_callback import *
-from airflow import DAG
-from airflow.providers.postgres.hooks.postgres import PostgresHook
-from airflow.providers.postgres.operators.postgres import PostgresOperator
-from airflow.operators.python import PythonOperator
-from datetime import datetime, timedelta
+import pytest
+from common.on_failure_callback import get_task_app_name
+
 
 PSYCOPG_CONNECTION_ID = os.getenv("AIRFLOW_CONN_POSTGRES_OPENLEDGER_TESTING")
 SLEEP_SQL_TEMPLATE = "SELECT PG_SLEEP({sleep_time});"
 logger = logging.getLogger(__name__)
 
+
 @pytest.mark.parametrize(
     "context, expected",
     [
         pytest.param("app_ID_value", "app_ID_value", id="happy_path_str"),
-        pytest.param({"task_instance_key_str": "app_ID_value"}, "app_ID_value", id="happy_path_dict"),
+        pytest.param(
+            {"task_instance_key_str": "app_ID_value"},
+            "app_ID_value",
+            id="happy_path_dict",
+        ),
     ],
 )
 def test_get_task_app_name(context, expected):
     actual = get_task_app_name(context)
     assert actual == expected
-    
+
+
 @pytest.mark.parametrize(
     "context",
     [
@@ -36,20 +37,30 @@ def test_get_task_app_name_type_error(context):
     with pytest.raises(TypeError):
         get_task_app_name(context)
 
+
 def test_get_task_app_name_index_error():
     context = {"exception": "raise exception"}
     with pytest.raises(IndexError):
         get_task_app_name(context)
 
+
 # # TO DO: Add tests for pg_terminator and integrated
 
-# # MORE TESTING: 
+# # MORE TESTING:
 # # I used the code commented out below to try to recreate the issue from airflow and
 # # check that things were deleted.
 # # To manually check what was left running, I used just db-shell and this SQL:
 # # select pid, application_name, left(query, 50), backend_start from pg_stat_activity order by backend_start;
 
 # # DAG FOR TESTING:
+
+# import psycopg2
+# from airflow import DAG
+# from airflow.operators.python import PythonOperator
+# from airflow.providers.postgres.hooks.postgres import PostgresHook
+# from airflow.providers.postgres.operators.postgres import PostgresOperator
+# from common.constants import POSTGRES_CONN_ID
+# from datetime import datetime, timedelta
 
 # def hook_sleeper(
 #     task_app_name,
@@ -58,7 +69,7 @@ def test_get_task_app_name_index_error():
 # ):
 #     pg = PostgresHook(postgres_conn_id)
 #     sql_string = (
-#         PG_SET_NAME_TEMPLATE.format(task_app_name) 
+#         PG_SET_NAME_TEMPLATE.format(task_app_name)
 #         + SLEEP_SQL_TEMPLATE.format(sleep_time = sleep_time)
 #     )
 #     logger.info(f"Running this SQL: {sql_string}")
@@ -72,14 +83,14 @@ def test_get_task_app_name_index_error():
 #     conn = psycopg2.connect(postgres_conn_id)
 #     cur = conn.cursor()
 #     sql_string = (
-#         PG_SET_NAME_TEMPLATE.format(task_app_name) 
+#         PG_SET_NAME_TEMPLATE.format(task_app_name)
 #         + SLEEP_SQL_TEMPLATE.format(sleep_time = sleep_time)
 #     )
 #     logger.info(f"Running this SQL: {sql_string}")
 #     cur.execute(sql_string)
 #     conn.commit()
 
-# with DAG("killer_pg_test", 
+# with DAG("killer_pg_test",
 #     start_date=datetime(2022, 9, 4),
 #     schedule_interval=None,
 # ) as dag:
@@ -88,7 +99,7 @@ def test_get_task_app_name_index_error():
 #         task_id = "create_tracking_tables",
 #         postgres_conn_id = POSTGRES_CONN_ID,
 #         sql = (
-#             PG_SET_NAME_TEMPLATE.format(get_task_app_name({{ task_instance_key_str }})) 
+#             PG_SET_NAME_TEMPLATE.format(get_task_app_name({{ task_instance_key_str }}))
 #             + "CREATE TABLE IF NOT EXISTS db_job_status AS ("
 #             + "SELECT now() as check_timestamp, pid, usename, application_name, backend_start, query_start, query, state "
 #             + "FROM pg_stat_activity LIMIT 0"
@@ -102,7 +113,7 @@ def test_get_task_app_name_index_error():
 #         task_id = "pg_operator_sleep_30",
 #         postgres_conn_id = POSTGRES_CONN_ID,
 #         sql = (
-#             PG_SET_NAME_TEMPLATE.format(get_task_app_name({{ task_instance_key_str }})) 
+#             PG_SET_NAME_TEMPLATE.format(get_task_app_name({{ task_instance_key_str }}))
 #             + SLEEP_SQL_TEMPLATE.format(sleep_time = 30)
 #         ),
 #         retries = 0,
@@ -132,7 +143,7 @@ def test_get_task_app_name_index_error():
 #         task_id = "get_db_status",
 #         postgres_conn_id = POSTGRES_CONN_ID,
 #         sql = (
-#             PG_SET_NAME_TEMPLATE.format(get_task_app_name({{ task_instance_key_str }})) 
+#             PG_SET_NAME_TEMPLATE.format(get_task_app_name({{ task_instance_key_str }}))
 #             + "INSERT INTO db_job_status ("
 #             + "SELECT now() as check_timestamp, pid, usename, application_name, backend_start, query_start, query, state "
 #             + "FROM pg_stat_activity WHERE query != '<IDLE>' AND query NOT ILIKE '%pg_stat_activity%' "
@@ -145,12 +156,12 @@ def test_get_task_app_name_index_error():
 
 #     (
 #         create_tracking_table
-#         >> 
+#         >>
 #         [
-#             pg_operator_sleep_30, 
+#             pg_operator_sleep_30,
 #             pg_hook_sleep_30,
 #             psycopg2_sleep_30
-#         ] 
-#         >> 
+#         ]
+#         >>
 #         get_db_status
 #     )
