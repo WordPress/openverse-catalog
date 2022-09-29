9e6fc9360abf09e16bf6e4e82c38151a213a5b5f
Madison Swain-Bowden <bowdenm@spu.edu>
2022-09-28 17:36:58 -0700

    Refactor other functions

--- openverse_catalog/dags/providers/provider_api_scripts/freesound.py
+++ openverse_catalog/dags/providers/provider_api_scripts/freesound.py
@@ -8,8 +8,9 @@ Output:                 TSV file containing the image, the respective
 
 Notes:                  https://freesound.org/apiv2/search/text'
                         No rate limit specified.
+                        This script can be run either to ingest the full dataset or
+                        as a dated DAG.
 """
-import copy
 import functools
 import logging
 from datetime import datetime
@@ -65,6 +66,14 @@ class FreesoundDataIngester(ProviderDataIngester):
     def get_next_query_params(self, prev_query_params: dict | None, **kwargs) -> dict:
         license_name = kwargs.get("license_name")
         if not prev_query_params:
+            start_date = "*"
+            # Allow self.date to be undefined, necessary for the first full, successful
+            # run of Freesound but can be changed to dated afterwards.
+            # TODO: Madison you need to mention this in the PR description :)
+            if self.date:
+                start_date = datetime.strftime(
+                    datetime.fromisoformat(self.date), "%Y-%m-%dT%H:%M:%SZ"
+                )
             return {
                 "format": "json",
                 "token": self.api_key,
@@ -74,6 +83,7 @@ class FreesoundDataIngester(ProviderDataIngester):
                 "filesize,bitrate,bitdepth,duration,samplerate,pack,username,"
                 "num_downloads,avg_rating,num_ratings,geotag,previews",
                 "license_name": license_name,
+                "filter": f"created:[{start_date} TO NOW]",
                 "page": 1,
             }
         else:
@@ -81,9 +91,9 @@ class FreesoundDataIngester(ProviderDataIngester):
 
     def get_batch_data(self, response_json):
         if response_json:
-            return response_json.get("results")
+            # Freesound sometimes returns results that are just "None", filter these out
+            return [item for item in response_json.get("results") if item is not None]
         return None
-
 
     @staticmethod
     def _get_creator_data(item):
@@ -93,7 +103,6 @@ class FreesoundDataIngester(ProviderDataIngester):
         else:
             creator_url = None
         return creator, creator_url
-
 
     @staticmethod
     def _get_metadata(item):
@@ -111,7 +120,6 @@ class FreesoundDataIngester(ProviderDataIngester):
                 metadata[field] = field_value
         return metadata
 
-
     @staticmethod
     def _get_license(item):
         item_license = get_license_info(license_url=item.get("license"))
@@ -120,207 +128,130 @@ class FreesoundDataIngester(ProviderDataIngester):
             return None
         return item_license
 
-
-def main(date="all"):
-    """This script pulls the data for a given date from the Freesound,
-    and writes it into a .TSV file to be eventually read
-    into our DB.
-
-    Required Arguments:
-
-    date:  Date String in the form YYYY-MM-DD.  This is the date for
-           which running the script will pull data.
-    """
-
-    logger.info("Begin: Freesound script")
-    licenses = ["Attribution", "Attribution Noncommercial", "Creative Commons 0"]
-    for license_name in licenses:
-        audio_count = _get_items(license_name, date)
-        logger.info(f"Audios for {license_name} pulled: {audio_count}")
-    logger.info("Terminated!")
-
-
-def _get_query_params(
-    license_name="",
-    page_number=1,
-    default_query_params=None,
-):
-    default_query_params = default_query_params or DEFAULT_QUERY_PARAMS
-    return {
-        **default_query_params,
-        "page": str(page_number),
-        "license": license_name,
-    }
-
-
-def _get_items(license_name, date):
-    item_count = 0
-    page_number = 1
-    should_continue = True
-    default_query_params = copy.deepcopy(DEFAULT_QUERY_PARAMS)
-    try:
-        start_date = datetime.strftime(
-            datetime.fromisoformat(date), "%Y-%m-%dT%H:%M:%SZ"
+    @functools.lru_cache(maxsize=1024)
+    def _get_set_info(self, set_url):
+        response_json = self.get_response_json(
+            query_params={"token": self.api_key},
+            endpoint=set_url,
         )
-    except ValueError:
-        start_date = "*"
-    default_query_params["filter"] = f"created:[{start_date} TO NOW]"
-    while should_continue:
-        query_param = _get_query_params(
-            default_query_params=default_query_params,
-            license_name=license_name,
-            page_number=page_number,
-        )
-        batch_data = _get_batch_json(query_param=query_param)
-        if batch_data:
-            item_count = _process_item_batch(batch_data)
-            page_number += 1
+        set_id = response_json.get("id")
+        set_name = response_json.get("name")
+        return set_id, set_name
+
+    def _get_audio_set_info(self, media_data):
+        # set id, set name, set url
+        set_url = media_data.get("pack")
+        if set_url is not None:
+            set_id, set_name = self._get_set_info(set_url)
+            return set_id, set_name, set_url
         else:
-            should_continue = False
-    return item_count
+            return None, None, None
 
-
-def _get_batch_json(endpoint=ENDPOINT, headers=None, retries=RETRIES, query_param=None):
-    if headers is None:
-        headers = HEADERS
-    response_json = delayed_requester.get_response_json(
-        endpoint, retries, query_param, headers=headers
-    )
-    if response_json is None:
-        return None
-    else:
-        results = response_json.get("results")
-        return results
-
-
-def _process_item_batch(items_batch):
-    for item in items_batch:
-        # Freesound sometimes returns results that are just "None"
-        if item is None:
-            continue
-        item_meta_data = _extract_audio_data(item)
-        if item_meta_data is None:
-            continue
-        audio_store.add_item(**item_meta_data)
-    return audio_store.total_items
-
-
-def _extract_audio_data(media_data):
-    """Extracts meta data about the audio file
-    Freesound does not have audio thumbnails"""
-    foreign_landing_url = media_data.get("url")
-    if not foreign_landing_url:
-        return None
-
-    foreign_identifier = media_data.get("id")
-    if not foreign_identifier:
-        return None
-
-    item_license = _get_license(media_data)
-    if item_license is None:
-        return None
-
-    # We use the mp3-hq preview url as `audio_url` as the main url
-    # for playing on the frontend,
-    # and the actual uploaded file as an alt_file that is available
-    # for download (and requires a user to be authenticated to download)
-    try:
-        main_audio, alt_files = _get_audio_files(media_data)
-    except FLAKY_EXCEPTIONS:
-        logger.warning(f"Unable to get file size for {foreign_landing_url}, skipping")
-        return None
-    if main_audio is None:
-        return None
-
-    creator, creator_url = _get_creator_data(media_data)
-    duration = int(media_data.get("duration") * 1000)
-    set_foreign_id, audio_set, set_url = _get_audio_set_info(media_data)
-    return {
-        "title": media_data.get("name"),
-        "creator": creator,
-        "creator_url": creator_url,
-        "foreign_identifier": foreign_identifier,
-        "foreign_landing_url": foreign_landing_url,
-        "duration": duration,
-        "license_info": item_license,
-        "meta_data": _get_metadata(media_data),
-        "raw_tags": media_data.get("tags"),
-        "set_foreign_id": set_foreign_id,
-        "audio_set": audio_set,
-        "set_url": set_url,
-        "alt_files": alt_files,
-        # audio_url, filetype, bit_rate
-        **main_audio,
-    }
-
-
-def _get_audio_set_info(media_data):
-    # set id, set name, set url
-    set_url = media_data.get("pack")
-    if set_url is not None:
-        set_id, set_name = _get_set_info(set_url)
-        return set_id, set_name, set_url
-    else:
-        return None, None, None
-
-
-@functools.lru_cache(maxsize=1024)
-def _get_set_info(set_url):
-    response_json = delayed_requester.get_response_json(
-        set_url,
-        3,
-        query_params={"token": API_KEY},
-    )
-    set_id = response_json.get("id")
-    set_name = response_json.get("name")
-    return set_id, set_name
-
-
-def _get_preview_filedata(preview_type, preview_url):
-    return {
-        "url": preview_url,
-        "filetype": preview_type.split("-")[-1],
-        "bit_rate": preview_bitrates[preview_type],
-    }
-
-
-@retry(FLAKY_EXCEPTIONS, tries=3, delay=1, backoff=2)
-def _get_audio_file_size(url):
-    """
-    Get the content length of a provided URL.
-    Freesound can be finicky, so we want to retry it a few times on these conditions:
-      * SSLError - 'EOF occurred in violation of protocol (_ssl.c:1129)'
-      * ConnectionError - '[Errno 113] No route to host'
-
-    Both of these seem transient and may be the result of some odd behavior on the
-    Freesound API end. We have an API key that's supposed to be maxed out, so I can't
-    imagine it's throttling (aetherunbound).
-
-    TODO(obulat): move filesize detection to the polite crawler
-    """
-    return int(requests.head(url).headers["content-length"])
-
-
-def _get_audio_files(media_data):
-    # This is the original file, needs auth for downloading.
-    # bit_rate in kilobytes, converted to bytes
-    alt_files = [
-        {
-            "url": media_data.get("download"),
-            "bit_rate": int(media_data.get("bitrate")) * 1000,
-            "sample_rate": int(media_data.get("samplerate")),
-            "filetype": media_data.get("type"),
-            "filesize": media_data.get("filesize"),
+    @staticmethod
+    def _get_preview_filedata(preview_type, preview_url):
+        return {
+            "url": preview_url,
+            "filetype": preview_type.split("-")[-1],
+            "bit_rate": FreesoundDataIngester.preview_bitrates[preview_type],
         }
-    ]
-    previews = media_data.get("previews")
-    # If there are no previews, then we will not be able to play the file
-    if not previews:
-        return None
-    main_file = _get_preview_filedata("preview-hq-mp3", previews["preview-hq-mp3"])
-    main_file["audio_url"] = main_file.pop("url")
-    main_file["filesize"] = _get_audio_file_size(main_file["audio_url"])
-    return main_file, alt_files
+
+    @retry(flaky_exceptions, tries=3, delay=1, backoff=2)
+    def _get_audio_file_size(self, url):
+        """
+        Get the content length of a provided URL.
+        Freesound can be finicky, so we want to retry it a few times on
+        these conditions:
+          * SSLError - 'EOF occurred in violation of protocol (_ssl.c:1129)'
+          * ConnectionError - '[Errno 113] No route to host'
+
+        Both of these seem transient and may be the result of some odd behavior on the
+        Freesound API end. We have an API key that's supposed to be maxed out, so
+        I can't imagine it's throttling (aetherunbound).
+
+        TODO(obulat): move filesize detection to the polite crawler
+        """
+        return int(requests.head(url).headers["content-length"])
+
+    def _get_audio_files(self, media_data):
+        # This is the original file, needs auth for downloading.
+        # bit_rate in kilobytes, converted to bytes
+        alt_files = [
+            {
+                "url": media_data.get("download"),
+                "bit_rate": int(media_data.get("bitrate")) * 1000,
+                "sample_rate": int(media_data.get("samplerate")),
+                "filetype": media_data.get("type"),
+                "filesize": media_data.get("filesize"),
+            }
+        ]
+        previews = media_data.get("previews")
+        # If there are no previews, then we will not be able to play the file
+        if not previews:
+            return None
+        main_file = self._get_preview_filedata(
+            "preview-hq-mp3", previews["preview-hq-mp3"]
+        )
+        main_file["audio_url"] = main_file.pop("url")
+        main_file["filesize"] = self._get_audio_file_size(main_file["audio_url"])
+        return main_file, alt_files
+
+    def get_record_data(self, media_data: dict) -> dict | list[dict] | None:
+        """
+        Extracts metadata about the audio file.
+        Freesound does not have audio thumbnails.
+        """
+        foreign_landing_url = media_data.get("url")
+        if not foreign_landing_url:
+            return None
+
+        foreign_identifier = media_data.get("id")
+        if not foreign_identifier:
+            return None
+
+        item_license = self._get_license(media_data)
+        if item_license is None:
+            return None
+
+        # We use the mp3-hq preview url as `audio_url` as the main url
+        # for playing on the frontend,
+        # and the actual uploaded file as an alt_file that is available
+        # for download (and requires a user to be authenticated to download)
+        try:
+            main_audio, alt_files = self._get_audio_files(media_data)
+        except self.flaky_exceptions:
+            logger.warning(
+                f"Unable to get file size for {foreign_landing_url}, skipping"
+            )
+            return None
+        if main_audio is None:
+            return None
+
+        creator, creator_url = self._get_creator_data(media_data)
+        duration = int(media_data.get("duration") * 1000)
+        set_foreign_id, audio_set, set_url = self._get_audio_set_info(media_data)
+        return {
+            "title": media_data.get("name"),
+            "creator": creator,
+            "creator_url": creator_url,
+            "foreign_identifier": foreign_identifier,
+            "foreign_landing_url": foreign_landing_url,
+            "duration": duration,
+            "license_info": item_license,
+            "meta_data": self._get_metadata(media_data),
+            "raw_tags": media_data.get("tags"),
+            "set_foreign_id": set_foreign_id,
+            "audio_set": audio_set,
+            "set_url": set_url,
+            "alt_files": alt_files,
+            # audio_url, filetype, bit_rate
+            **main_audio,
+        }
+
+
+def main():
+    logger.info("Begin: Freesound provider script")
+    ingester = FreesoundDataIngester()
+    ingester.ingest_records()
 
 
 if __name__ == "__main__":
