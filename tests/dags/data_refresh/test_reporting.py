b9f9e1506271dfe543af49d5e0b55bb26f8624f7
Lakshmi Prasanna Sabbella <36727559+prasanna4567@users.noreply.github.com>
2022-10-18 22:10:36 +0530

    fix: updated tests

--- tests/dags/data_refresh/test_reporting.py
+++ tests/dags/data_refresh/test_reporting.py
@@ -5,10 +5,10 @@ from data_refresh.reporting import report_record_difference, report_status
 @pytest.mark.parametrize(
     "before, after, expected_in_message",
     [
-        ["1", "2", ["1 → 2", "+1 (+100.0%"]],
-        ["1", "3", ["1 → 3", "+2 (+200.0%"]],
-        ["4", "2", ["4 → 2", "-2 (-50.0%"]],
-        ["4000", "2000", ["4,000 → 2,000", "-2,000 (-50.0%"]],
+        ["1", "2", ["1 → 2", "+1 (+100.000%"]],
+        ["1", "3", ["1 → 3", "+2 (+200.000%"]],
+        ["4", "2", ["4 → 2", "-2 (-50.000%"]],
+        ["4000", "2000", ["4,000 → 2,000", "-2,000 (-50.000%"]],
     ],
 )
 def test_record_reporting(before, after, expected_in_message):
