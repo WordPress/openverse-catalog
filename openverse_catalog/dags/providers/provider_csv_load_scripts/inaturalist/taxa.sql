2ac15626aa4c777cbc14538cc8403da802de5c4e
rwidom <rebeccawidom@gmail.com>
2022-12-25 13:31:22 -0500

    clarify taxa comments and include languages

--- openverse_catalog/dags/providers/provider_csv_load_scripts/inaturalist/taxa.sql
+++ openverse_catalog/dags/providers/provider_csv_load_scripts/inaturalist/taxa.sql
@@ -11,17 +11,18 @@ Integrating data from the Catalog of Life to create titles and tags.
 Integrating data from iNaturalist taxonomy table with Catalog of Life name data.
 
 Title:
-    - If the scientific name has one or more English vernacular names, collect them into
-      the title, separated by commas.
+    - If the scientific name has one or more vernacular names, collect as many as will
+      fit into the title, separated by commas.
     - If not, there are a few taxa that are very common where I googled an English name
       and added it manually, so use that.
     - Otherwise, use the iNaturalist name. There are a few where the name is "Not
       assigned" but we're going to filter those records out & drop associated photos.
 
 Tags:
-    - If the title of a specific taxa is a vernacular name, put the iNaturalist name in
-      the tags.
-    - If there are non-English vernacular names, put them in the tags.
+    - If the title of a specific taxa is a vernacular name (from Catalog of Life or from
+      informal googling), put the iNaturalist name in the tags.
+    - If there are additional vernacular names that did not fit in the title, put them
+      in the tags.
     - Put the titles of ancestors in the tags.
     - Given the order of types of tags above plus alphabetical order, take only the
       first 20 tags.
@@ -31,7 +32,7 @@ TO DO #902: Find a DRYer way to do this enrichment with SQL
 */
 
 /*
-                   ********** Create tag type ***********
+                ********** Create tag data type ***********
 This at least makes the structure of tags a little more explicit in sql
 */
 DO $$ BEGIN
@@ -63,11 +64,11 @@ COMMIT;
                 ********** Integrate Catalog of Life Data *********
 
 + aggregate the catalog of life vernacular names table to the scientific name level,
-  with string lists of English names and JSON for tags in other languages
+  with string lists of names for titles and JSON arrays for tags
 + add in information from the manual table of common names and from inaturalist taxa,
   so that every taxon record has the best possible title
 + add in inaturalist taxa base data, with the category of "Life" excluded from
-  ancestry strings
+  inaturalist ancestry strings
 */
 drop table if exists inaturalist.taxa_with_vernacular;
 create table inaturalist.taxa_with_vernacular as
@@ -75,56 +76,70 @@ create table inaturalist.taxa_with_vernacular as
     with
     catalog_of_life_names as
     (
-        select cast(md5(n.scientificname) as uuid) as md5_scientificname,
+        select
+            /*
+            Assuming that it's more efficient to join on a uuid than a string up to 56
+            characters long.
+            */
+            cast(md5(n.scientificname) as uuid) as md5_scientificname,
             v.name_language,
             v.taxon_name,
-            /* This is prioritizing English vernacular names, and should be cut or
-            reorganized if the 20 tag limit is not a thing. */
-            row_number() OVER (partition by n.scientificname
-                order by case when v.name_language='eng' then 0 else 1 end,
-                v.taxon_name)
-                as name_rank,
-            count(case when v.name_language='eng' then 1 end)
-                OVER (partition by n.scientificname
-                rows between unbounded preceding and unbounded following)
-                as english_names
+            /*
+            name_string_length -- Cumulative length of the title string (taxon names
+                plus comma and space) up to and including the current name, within each
+                scientific name. There is no hierarchy / priority field within the
+                vernacular table, so the names are ordered with English names first, and
+                then alphabetical order.
+                In the target table the title can be no more than 5,000 characters long.
+                So, if there are vernacular names that would make the title more than
+                5,000 characters they will be used as tags instead of title below.
+            */
+            sum(length(v.taxon_name)+2) OVER (partition by n.scientificname
+                order by case when v.name_language='eng' then 0 else 1 end, v.taxon_name
+                rows between unbounded preceding and current row)
+                as name_string_length
         FROM inaturalist.col_name_usage n
             INNER JOIN inaturalist.col_vernacular v on v.taxonid = n.id
-        /* catalog of life seems to be integrating a number of different data sources
-        and none of the vernacular names have taxon ids with more than 10 characters,
-        so allowing postgres to filter before the join saves a little time. */
-        where length(n.id) <= 10
     ),
     catalog_of_life as
     (
         SELECT
             md5_scientificname,
+            /*
+            Put as many vernacular names as possible in the title, and put the rest in
+            tags.
+            */
             string_agg(DISTINCT
-                case when name_language = 'eng' then taxon_name end,
-                ', ') name_english,
-            jsonb_agg(DISTINCT cast((taxon_name, 'inaturalist') as openverse_tag))
-                FILTER (where name_rank > english_names) as tags_nonenglish_vernacular
+                case when name_string_length < 5000
+                then taxon_name end,
+                ', ') name_string,
+            array_agg(DISTINCT cast((taxon_name, 'inaturalist') as openverse_tag))
+                FILTER (where name_string_length > 5000)
+                as tags_vernacular
         FROM catalog_of_life_names
-        /* Don't keep more than 20 alternative names for any scientific name if we have
-        a limit of 20 tags per image. There are some extreme terms that would have
-        thousands of tags. */
-        where name_rank <= 20
         group by 1
     )
     select
         taxa.taxon_id,
         (case when ancestry='48460' then '' else replace(taxa.ancestry,'48460/','') end)
             as ancestry, --exclude 'Life' from ancestry
-        left(coalesce(
-            catalog_of_life.name_english,
-            manual_name_additions.vernacular_name, --name_manual,
-            taxa.name -- name_inaturalist,
-        ), 5000) as title,
-        (case when catalog_of_life.name_english is not null or
+        coalesce(
+            catalog_of_life.name_string, --string list of vernacular names
+            manual_name_additions.vernacular_name, --name_manual
+            taxa.name -- name_inaturalist
+        ) as title,
+        /*
+        If the inaturalist name will not be the title, get ready to add it as a tag.
+        */
+        (case when catalog_of_life.name_string is not null or
             manual_name_additions.vernacular_name is not null
             then to_jsonb(array_fill(cast((taxa.name, 'inaturalist') as openverse_tag), array[1]))
             end) inaturalist_name_tag,
-        tags_nonenglish_vernacular
+        /*
+        We don't want more than 20 tags total, so doesn't make sense to have more than
+        20 vernacular tags.
+        */
+        to_jsonb(tags_vernacular[1:20]) tags_vernacular
     from inaturalist.taxa
     LEFT JOIN catalog_of_life
         on (cast(md5(taxa.name) as uuid) = catalog_of_life.md5_scientificname)
@@ -180,22 +195,21 @@ create table inaturalist.taxa_enriched as
         child.title,
         jsonb_path_query_array(
             (
-                /* concatenating jsonb arrays works as long as you have an empty array
-                rather than a null::jsonb */
+                /*
+                concatenating jsonb arrays works as long as you have an empty array
+                rather than a null::jsonb
+                */
                 coalesce(child.inaturalist_name_tag, to_jsonb(array[]::openverse_tag[]))
                 || coalesce(jsonb_agg(DISTINCT
                     cast((ancestors.title,'inaturalist') as openverse_tag))
                     FILTER (where ancestors.title is not null),
                     to_jsonb(array[]::openverse_tag[]))
-                || coalesce(child.tags_nonenglish_vernacular,
+                || coalesce(child.tags_vernacular,
                     to_jsonb(array[]::openverse_tag[]))
             ),
             /*
             Use the jsonb query to retrieve only the first 20 values of the array that
-            combines vernacular and ancestor tags.
-            @AetherUnbound, I had thought that I saw code somewhere limiting to a max of
-            20 tags, but now I can't find that code. Should I leave this out? It would
-            definitely help with language access.
+            combines inaturalist, vernacular and ancestor tags.
             */
             '$[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]')
         as tags
@@ -204,7 +218,7 @@ create table inaturalist.taxa_enriched as
         left join inaturalist.taxa_with_vernacular ancestors
         on (ancestors.taxon_id = ANY (string_to_array(child.ancestry, '/')::int[]))
     group by child.taxon_id, child.title,
-        child.inaturalist_name_tag, child.tags_nonenglish_vernacular
+        child.inaturalist_name_tag, child.tags_vernacular
 );
 ALTER TABLE inaturalist.taxa_enriched ADD PRIMARY KEY (taxon_id);
 COMMIT;
