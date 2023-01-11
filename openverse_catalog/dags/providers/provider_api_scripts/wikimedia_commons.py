fdc06e026e4f05467a3168ad02dcd78ec9662a33
Mary Ann <maryannjose129@gmail.com>
2023-01-11 19:01:47 +0530

    removed delay=0.1

--- openverse_catalog/dags/providers/provider_api_scripts/wikimedia_commons.py
+++ openverse_catalog/dags/providers/provider_api_scripts/wikimedia_commons.py
@@ -46,7 +46,6 @@ class WikimediaCommonsDataIngester(ProviderDataIngester):
         "audio": prov.WIKIMEDIA_AUDIO_PROVIDER,
     }
     endpoint = f"https://{HOST}/w/api.php"
-    delay =0.1
     headers = {"User-Agent": prov.UA_STRING}
 
     # The batch_limit applies to the number of pages received by the API, rather
