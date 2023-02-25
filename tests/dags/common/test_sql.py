f741b61a2a21bffcdcf3b7fdf5b10af551558e33
Madison Swain-Bowden <bowdenm@spu.edu>
2023-02-24 16:26:56 -0800

    Also fix test associated with log message

--- tests/dags/common/test_sql.py
+++ tests/dags/common/test_sql.py
@@ -120,9 +120,7 @@ def test_PostgresHook_get_execution_timeout_no_task(caplog):
     actual_result = PostgresHook.get_execution_timeout(None)
     assert len(caplog.records) == 1
     assert caplog.records[0].levelname == "WARNING"
-    assert (
-        "Received None, not a task. Returning universal_default=3600.0" in caplog.text
-    )
+    assert "No task supplied, using default timeout of 3600.0s" in caplog.text
     assert actual_result == DEFAULT_TIMEOUT
 
 
