388706b2bfdb9264457ddd01238217c8fcb5df1d
rsubra13 <ramkisastra@gmail.com>
2023-03-20 14:31:54 -0700

    Use f'' strings

--- openverse_catalog/dags/common/loader/reporting.py
+++ openverse_catalog/dags/common/loader/reporting.py
@@ -53,7 +53,7 @@ def humanize_time_duration(seconds: float | int) -> str:
     for unit, div in TIME_DURATION_UNITS:
         amount, seconds = divmod(int(seconds), div)
         if amount > 0:
-            parts.append("{} {}{}".format(amount, unit, "" if amount == 1 else "s"))
+            parts.append(f'{amount} {unit}{"" if amount == 1 else "s"}')
     return ", ".join(parts)
 
 
