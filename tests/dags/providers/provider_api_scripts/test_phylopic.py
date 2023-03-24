4f998c15d1ea87a7926e459e1f3aa2e58fe7c7f4
Krystle Salazar <krystle.salazar@ciens.ucv.ve>
2023-03-24 16:22:57 -0400

    Test get_next_query_params

--- tests/dags/providers/provider_api_scripts/test_phylopic.py
+++ tests/dags/providers/provider_api_scripts/test_phylopic.py
@@ -29,3 +29,26 @@ def test__get_initial_query_params():
 
     assert pp.build_param == 194
     assert pp.total_pages == 145
+
+
+@pytest.mark.parametrize(
+    "build_param, current_page, expected_query_params",
+    [
+        (111, 0, {"build": 111, "page": 0}),
+        (222, 2, {"build": 222, "page": 3}),
+    ],
+)
+def test_get_next_query_params(build_param, current_page, expected_query_params):
+    pp.build_param = build_param
+    pp.current_page = current_page
+
+    prev_query_params = None
+    if current_page != 0:
+        prev_query_params = {
+            "build": pp.build_param,
+            "page": pp.current_page,
+            "embed_items": "true",
+        }
+    actual_query_params = pp.get_next_query_params(prev_query_params)
+
+    assert actual_query_params == expected_query_params | {"embed_items": "true"}
