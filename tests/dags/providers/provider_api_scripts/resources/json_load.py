6ce52dae56a853d1914b3acec79810b63e522f30
Will Brown <williamlandisbrown@gmail.com>
2023-03-27 20:28:53 -0400

    Remove FakeSource.py

--- tests/dags/providers/provider_api_scripts/resources/json_load.py
+++ tests/dags/providers/provider_api_scripts/resources/json_load.py
@@ -5,14 +5,23 @@ from pathlib import Path
 
 def make_resource_json_func(folder_name: str) -> Callable[[str], dict]:
     """
-    _summary_
+    Given the name of a folder inside the resources folder returns
+    a function that takes a json file name within the given folder
+    and returns the contents of that json file as a dictionary.
 
     Args:
-        folder_name (str): _description_
+        folder_name (str): name of a folder inside the resources folder
+        (tests/dags/providers/provider_api_scripts/resources/[folder_name]).
 
     Returns:
         Callable[[str], dict]: A function that when given a json
-        resource name as a string returns it as a dictionary.
+        resource name as a string returns that json resource as a dictionary.
+
+    Example:
+        json_func_ex = make_resource_json_func("brooklynmuseum")
+        json_dict = json_func_ex("cc_license_info.json")
+        assert json_dict["public_name"] == "Creative Commons-BY" # True
+
     """
     resources = Path(__file__).parent / folder_name
 
